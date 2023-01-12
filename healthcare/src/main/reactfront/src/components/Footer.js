acecfde75752f3a5dc88ff22a22573c67a78ca63
JoSihun <kepler452k@naver.com>
2023-01-12 23:25:08 +0900

    feat(reactfront): add basic components install required libraries npm install axios npm install react-router-dom npm install react-bootstrap bootstrap
    add Navigation.js in ./reactfront/src/components
    add Header.js in ./reactfront/src/components
    
    add Body.js in ./reactfront/src/components
    add Body.css in ./reactfront/src/components
    
    add Footer.js in ./reactfront/src/components
    add Footer.css in ./reactfront/src/components

---
+++ healthcare/src/main/reactfront/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="Footer" fixed="bottom">
+            <div>This is Footer.</div>
+        </div>
+    );
+}
+
+export default Footer;
