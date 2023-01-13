f2f7c8a6eb38592f2fe62b9f3a3b25c4c6961868
JoSihun <kepler452k@naver.com>
2023-01-13 23:18:46 +0900

    feat(reactfront): add Navigation.css

--- healthcare/src/main/reactfront/src/components/Navigation.js
+++ healthcare/src/main/reactfront/src/components/Navigation.js
@@ -2,14 +2,15 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Navigation.css'
 
 function Navigation() {
     return (
         <div className="Navigation">
             <Navbar bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="me-auto">
+                    <Navbar.Brand href="#home">HealthCare</Navbar.Brand>
+                    <Nav className="menu">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#features">Features</Nav.Link>
                         <Nav.Link href="#pricing">Pricing</Nav.Link>
