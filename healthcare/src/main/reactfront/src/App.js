6bdd484559ac51d452ccba8e6be9bae7dd565cb4
JoSihun <kepler452k@naver.com>
2023-01-17 17:40:30 +0900

    feat(reactfront/Support.js): design front with react bootstrap

--- healthcare/src/main/reactfront/src/App.js
+++ healthcare/src/main/reactfront/src/App.js
@@ -13,7 +13,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-      <div className="App">
+      <div>
           <BrowserRouter>
               <Navigation />
               <Banner />
