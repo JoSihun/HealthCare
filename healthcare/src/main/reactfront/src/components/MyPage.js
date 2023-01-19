51e0f32def22dbb16577200da96cc0949d273cdc
JoSihun <kepler452k@naver.com>
2023-01-19 17:56:01 +0900

    feat(MyPage.js): add skeleton frame for mypage with Bootstrap Template add profile image frame for front-end add profile info frame for front-end

--- healthcare/src/main/reactfront/src/components/MyPage.js
+++ healthcare/src/main/reactfront/src/components/MyPage.js
@@ -1,12 +1,49 @@
+import bg_black from '../img/bg_black.jpg';
 import React from "react";
+import {Card, Col, Row} from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 
 function MyPage() {
     return (
-        <div className="MyPage">
-            <div>
-                <h1>This is MyPage.</h1>
-            </div>
-        </div>
+        <>
+            <Container fluid className="min-vh-100">
+                <Row className="my-3">
+                    <Col className="col-md-12">
+                        <Card className="min-vh-100">
+                            <Card.Body>
+                                <Card.Title>
+                                    <div className="">
+                                        <div className="d-inline-flex d-inline-block me-md-2">
+                                            <img
+                                                className="rounded-circle"
+                                                src={bg_black}
+                                                width="100"
+                                                height="100"
+                                                alt="profile-image"
+                                            />
+                                        </div>
+                                        <div className="d-inline-block mx-md-2">
+                                            <div>Name: Profile</div>
+                                            <div>Birth: 1990.01.01</div>
+                                            <div>Height: 183cm</div>
+                                            <div>Weight: 75kg</div>
+                                        </div>
+                                    </div>
+
+                                </Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                                <Card.Link href="#">Card Link</Card.Link>
+                                <Card.Link href="#">Another Link</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </>
     );
 }
 
