aa8fbb18b664b5afb0ff8af5acddc27cf4f92502
JoSihun <kepler452k@naver.com>
2023-01-24 16:46:47 +0900

    feat(reactfront): add my-page skeleton front structure

--- healthcare/src/main/reactfront/src/components/MyPage.js
+++ healthcare/src/main/reactfront/src/components/MyPage.js
@@ -6,38 +6,129 @@ import Container from "react-bootstrap/Container";
 function MyPage() {
     return (
         <>
-            <Container fluid className="min-vh-100">
-                <Row className="my-3">
-                    <Col className="col-md-12">
-                        <Card className="min-vh-100">
+            <Container fluid>
+                <Row className="my-md-3">
+                    <Col className="col-md-6">
+                        <Card className="h-100">
                             <Card.Body>
-                                <Card.Title>
-                                    <div className="">
-                                        <div className="d-inline-flex d-inline-block me-md-2">
-                                            <img
-                                                className="rounded-circle"
-                                                src={bg_black}
-                                                width="100"
-                                                height="100"
-                                                alt="profile-image"
-                                            />
-                                        </div>
-                                        <div className="d-inline-block mx-md-2">
-                                            <div>Name: Profile</div>
-                                            <div>Birth: 1990.01.01</div>
-                                            <div>Height: 183cm</div>
-                                            <div>Weight: 75kg</div>
-                                        </div>
-                                    </div>
-
-                                </Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                <Row>
+                                    <Col className="col-md-2">
+                                        <img
+                                            className="rounded-circle"
+                                            src={bg_black}
+                                            width="100"
+                                            height="100"
+                                            alt="profile-image"
+                                        />
+                                    </Col>
+                                    <Col className="col-md-10">
+                                        <div>이름: Profile</div>
+                                        <div>생년월일: 2000.01.01</div>
+                                        <div>신장/체중: 180cm / 75kg</div>
+                                        <div><h3><b><u>프로필 인적사항 미확정</u></b></h3></div>
+                                    </Col>
+                                </Row>
+                                <hr/>
+                                <Card.Title>This is Card Title (Purpose & Goal or Something)</Card.Title>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
+                                    This is Card Text (Like Target BMI)<br/>
+                                    This is Card Text (Like Target Weight)<br/>
+                                    This is Card Text (Like Target Fat Mass)<br/>
+                                    This is Card Text (Like Target Muscle Mass)<br/>
                                 </Card.Text>
-                                <Card.Link href="#">Card Link</Card.Link>
-                                <Card.Link href="#">Another Link</Card.Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className="col-md-3">
+                        <Card className="h-100">
+                            <Card.Body>
+                                <div className="d-flex justify-content-center">
+                                    <img
+                                        className="rounded-circle"
+                                        src={bg_black}
+                                        width="200"
+                                        height="200"
+                                        alt="profile-image"
+                                    />
+                                </div>
+                                <hr/>
+                                <Card.Title>This is Card Title (BMI Graph)</Card.Title>
+                                <Card.Text>
+                                    This is Card Text (Not Decided)<br/>
+                                    This is Card Text (Maybe BMI지수?)<br/>
+                                    This is Card Text (Maybe 골격근량?)<br/>
+                                    This is Card Text (Maybe 체지방량?)<br/>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className="col-md-3">
+                        <Card className="h-100">
+                            <Card.Body>
+                                <Card.Title>This is Card Title (BMI Details)</Card.Title><hr/>
+                                <Card.Text>
+                                    BMI Detail 1: This is Card Text (Maybe BMI지수?)<br/>
+                                    BMI Detail 2: This is Card Text (Maybe 비만여부?)<br/>
+                                    BMI Detail 3: This is Card Text (Maybe 골격근량?)<br/>
+                                    BMI Detail 4: This is Card Text (Maybe 체지방량?)<br/>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+
+                <Row className="my-md-3">
+                    <Col className="col-md-6">
+                        <Card className="h-100">
+                            <Card.Body>
+                                <div className="d-flex justify-content-center">
+                                    <img
+                                        className="rounded"
+                                        src={bg_black}
+                                        width="800"
+                                        height="400"
+                                        alt="undefined-image"
+                                    />
+                                </div>
+                                <hr/>
+                                <Card.Title>This is Card Title (Calendar or Something)</Card.Title>
+                                <Card.Text>
+                                    This is Card Text (Not Decided)<br/>
+                                    This is Card Text (Not Decided)<br/>
+                                    This is Card Text (Not Decided)<br/>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className="col-md-3">
+                        <Card className="h-100">
+                            <Card.Body>
+                                <Card.Title>This is Card Title (Recommended Routine)</Card.Title><hr/>
+                                <Card.Text>
+                                    Routine 1: This is Card Text<br/>
+                                    Routine 2: This is Card Text<br/>
+                                    Routine 3: This is Card Text<br/>
+                                    Routine 4: This is Card Text<br/>
+                                    Routine 5: This is Card Text<br/>
+                                    Routine 6: This is Card Text<br/>
+                                    Routine 7: This is Card Text<br/>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col className="col-md-3">
+                        <Card className="h-100">
+                            <Card.Body>
+                                <Card.Title>This is Card Title (Recommended Diet)</Card.Title><hr/>
+                                <Card.Text>
+                                    Diet Menu 1: This is Card Text<br/>
+                                    Diet Menu 2: This is Card Text<br/>
+                                    Diet Menu 3: This is Card Text<br/>
+                                    Diet Menu 4: This is Card Text<br/>
+                                    Diet Menu 5: This is Card Text<br/>
+                                    Diet Menu 6: This is Card Text<br/>
+                                    Diet Menu 7: This is Card Text<br/>
+                                </Card.Text>
                             </Card.Body>
                         </Card>
                     </Col>
