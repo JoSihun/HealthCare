ea5d181bd7c3cfd902c3e8afffbc188293d9621f
jyd12190@gmail.com <jyd12190@gmail.com>
2023-03-24 13:14:09 +0900

    feat(Facility.js): interface changed

--- healthcare/src/main/reactfront/src/pages/introduce/Facility.js
+++ healthcare/src/main/reactfront/src/pages/introduce/Facility.js
@@ -1,75 +1,57 @@
 import '../../styles/Facility.css'
-import { Row, Col } from "react-bootstrap";
-import React from "react";
-import {Link} from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Button, Card, Row, Col, Container } from "react-bootstrap";
+import { Link, useSearchParams } from "react-router-dom";
+import SideBar from "../../components/introduce/SideBar";
 //SideBar 참고: https://citylock77.tistory.com/130
 
-function Facility() {
+export default function Facility() {
+    const [Facility, setFacility] = useState([]);
+
+    useEffect(() => {
+        const axiosGetFacility = async () => {
+            await axios.get(`/introduce/facility`)
+            .then((response) => {
+                setFacility(response.data);
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
+
+        axiosGetFacility();
+    }, []);
+
     return (
-        <div className="Facilities">
-            <container fluid>
-                <Row className="justify-content-center vh-100">
-                    <Col className="col-md-2 m-4">
-                        <Row className="p-2 sidebar">
-                            <div className="sidebarTitle">Introduce</div>
-                                <hr/>
-                                <Link to="/introduce/facilities" style={{ textDecoration: 'none' }}><div className="sidebarItem active">- Facilities</div></Link>
-                                <Link to="/introduce/staff" style={{ textDecoration: 'none' }}><div className="sidebarItem">- Staff</div></Link>
-                                <Link to="#Direction" style={{ textDecoration: 'none' }}><div className="sidebarItem">- Direction</div></Link>
-                        </Row>
-                    </Col>
-                    <Col className="col-md-8 m-4">
-                        <Row className="p-4 content h-100 align-content-start">
-                            <h1><b>Facilities</b></h1>
+        <Container fluid>
+            <Row className="justify-content-center">
+                <Col className="col-md-2 mx-2 my-4">
+                    <SideBar />
+                </Col>
+
+                <Col className="col-md-9 mx-2 my-4">
+                    <Card>
+                        <Card.Body>
+                            <Card.Title><h2><strong>Facility</strong></h2></Card.Title>
                             <hr/>
-                                <Row className="my-4">
-                                    <Col className="1">
-                                        <h1>Image1</h1>
-                                    </Col>
-                                    <Col className="2">
-                                        <h2>Sector1</h2>
-                                        <hr/>
-                                        <h5>
-                                            A. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                        </h5>
-                                    </Col>
-                                </Row>
-                                <Row className="my-5">
-                                    <Col className="1">
-                                        <h1>Image2</h1>
-                                    </Col>
-                                    <Col className="2">
-                                        <h2>Sector2</h2>
-                                        <hr/>
-                                        <h5>
-                                            A. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                        </h5>
-                                    </Col>
-                                </Row>
-                                <Row className="my-5">
-                                    <Col className="1">
-                                        <h1>Image3</h1>
-                                    </Col>
-                                    <Col className="2">
-                                        <h2>Sector3</h2>
-                                        <hr/>
-                                        <h5>
-                                            A. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                                        </h5>
-                                    </Col>
-                                </Row>
-                        </Row>
-                    </Col>
-                </Row>
-            </container>
-        </div>
+
+                            {Facility.map((facility, index) => (
+                                <div>
+                                    <div>{facility.sectorName}</div>
+                                    <div>{facility.sectorInfo}</div>
+                                    <div>{facility.sectorImg}</div>
+                                </div>
+                            ))}
+
+                            {/* <div className="d-flex justify-content-end">
+                                <Link to={"/support/freeboard/form"}>
+                                    <Button variant="dark" style={{ width: "100px" }}>글쓰기</Button>
+                                </Link>
+                            </div> */}
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 }
-
-export default Facility;
