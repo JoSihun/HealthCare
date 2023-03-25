cf6d6767d583cc0429dcbcf08138b8185d5272f4
jyd12190@gmail.com <jyd12190@gmail.com>
2023-03-25 17:55:54 +0900

    feat(reactfront): interface overall changed

--- healthcare/src/main/reactfront/src/pages/introduce/Facility.js
+++ healthcare/src/main/reactfront/src/pages/introduce/Facility.js
@@ -1,3 +1,5 @@
+import bg_black from '../../assets/images/bg_black.jpg'
+import bg_red from '../../assets/images/bg_red.png'
 import '../../styles/Facility.css'
 import axios from "axios";
 import React, { useEffect, useState } from "react";
@@ -6,20 +8,48 @@ import { Link, useSearchParams } from "react-router-dom";
 import SideBar from "../../components/introduce/SideBar";
 //SideBar 참고: https://citylock77.tistory.com/130
 
+const FacilityItem = (props) => {
+    const { facility } = props;
+
+    return (
+        <div className="d-flex justify-content-start mt-4 mb-2">
+            <div className="me-2">
+                <img
+                    className="rounded"
+                    src={bg_black}
+                    width="100%"
+                    height="100%"
+                    alt="profile"
+                    maxWidth="100px"
+                    // style={{ maxWidth: "100", maxHeight: "100" }} https://www.youtube.com/watch?v=QvQsuAaUwxo 참고
+                />
+            </div>
+            <div className="flex-fill ms-2">
+                <div><h2>{facility.sectorName}</h2></div>
+                <hr/>
+                <div>{facility.sectorInfo}</div>
+                <button>
+                    asdasd
+                </button>
+            </div>
+        </div>
+    );
+}
+
 export default function Facility() {
-    const [Facility, setFacility] = useState([]);
+    const [facilities, setFacilities] = useState([]);
 
     useEffect(() => {
-        const axiosGetFacility = async () => {
+        const axiosGetFacilities = async () => {
             await axios.get(`/introduce/facility`)
             .then((response) => {
-                setFacility(response.data);
+                setFacilities(response.data);
             }).catch((error) => {
                 console.log(error);
             });
         }
 
-        axiosGetFacility();
+        axiosGetFacilities();
     }, []);
 
     return (
@@ -34,20 +64,9 @@ export default function Facility() {
                         <Card.Body>
                             <Card.Title><h2><strong>Facility</strong></h2></Card.Title>
                             <hr/>
-                            
-                            {Facility.map((facility, index) => (
-                                <div>
-                                    <div>{facility.sectorName}</div>
-                                    <div>{facility.sectorInfo}</div>
-                                    <div>{facility.sectorImg}</div>
-                                </div>
-                            ))}
-
-                            {/* <div className="d-flex justify-content-end">
-                                <Link to={"/support/freeboard/form"}>
-                                    <Button variant="dark" style={{ width: "100px" }}>글쓰기</Button>
-                                </Link>
-                            </div> */}
+                            {facilities.map((facility, index) => (
+                                <FacilityItem facility={facility} />    
+                            ))}                          
                         </Card.Body>
                     </Card>
                 </Col>
