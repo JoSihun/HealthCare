3ea9b2865a3a56a171b09f4d43e0a9b88f082977
JoSihun <kepler452k@naver.com>
2023-03-29 17:04:04 +0900

    feat(PostRestController.java): add basic RESTful API that mapping urls contain 'v1' As APIs added, existing APIs mapping url contains 'v2'

--- healthcare/src/main/java/com/shyd/healthcare/controller/support/PostRestController.java
+++ healthcare/src/main/java/com/shyd/healthcare/controller/support/PostRestController.java
@@ -17,22 +17,45 @@ public class PostRestController {
     private final PostService postService;
     private final AttachmentService attachmentService;
 
-    @PostMapping("/api/post")
-    public Long postSave(@RequestBody PostSaveRequestDto requestDto,
+    /** POST REQUEST - header: "application/x-www-form-urlencoded" */
+    @PostMapping("/api/v1/post")
+    public Long savePost(@RequestBody PostSaveRequestDto requestDto) {
+        return this.postService.save(requestDto);
+    }
+
+    /** PUT REQUEST - header: "application/x-www-form-urlencoded" */
+    @PutMapping("/api/v1/post/{id}")
+    public Long updatePost(@PathVariable(value = "id") Long id,
+                           @RequestBody PostUpdateRequestDto requestDto) {
+        return this.postService.update(id, requestDto);
+    }
+
+    /** DELETE REQUEST - header: "application/x-www-form-urlencoded" */
+    @DeleteMapping("/api/v1/post/{id}")
+    public void deletePost(@PathVariable(value = "id") Long id) {
+        this.postService.delete(id);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /** POST REQUEST - header: "multipart/form-data" */
+    @PostMapping("/api/v2/post")
+    public Long postSave(@RequestPart(value = "data") PostSaveRequestDto requestDto,
                          @RequestPart(value = "files", required = false) List<MultipartFile> files) throws IOException {
         Long postId = this.postService.save(requestDto);
         return this.attachmentService.save(postId, files);
     }
 
-    @PutMapping("/api/post/{id}")
+    /** PUT REQUEST - header: "multipart/form-data" */
+    @PutMapping("/api/v2/post/{id}")
     public Long postUpdate(@PathVariable(value = "id") Long postId,
-                           @RequestBody PostUpdateRequestDto requestDto,
+                           @RequestPart(value = "data") PostUpdateRequestDto requestDto,
                            @RequestPart(value = "files", required = false) List<MultipartFile> files) throws IOException {
         this.attachmentService.update(postId, files);
         return this.postService.update(postId, requestDto);
     }
 
-    @DeleteMapping("/api/post/{id}")
+    /** DELETE REQUEST - header: "multipart/form-data" */
+    @DeleteMapping("/api/v2/post/{id}")
     public Long postDelete(@PathVariable Long id) {
         this.attachmentService.deleteAllFilesByPostId(id);
         return this.postService.delete(id);
