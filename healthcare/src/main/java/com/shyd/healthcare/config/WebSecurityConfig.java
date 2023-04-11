ba24bf9bd1c80a5cbea986416ba552dbb2eaf3af
JoSihun <kepler452k@naver.com>
2023-04-12 01:00:52 +0900

    feat(JWT): implement functions of JWT Authentication add websecurity and jwt in build.gradle add variables for JWT in application.properties
    implements CustomUserDetails.java
    implements CustomUserDetailsService.java
    
    add JwtTokenProvider.java
    add JwtTokenFilter.java
    add WebSecurityConfig.java

---
+++ healthcare/src/main/java/com/shyd/healthcare/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.shyd.healthcare.config;
+
+import com.shyd.healthcare.domain.user.CustomUserDetails;
+import com.shyd.healthcare.service.user.CustomUserDetailsService;
+import com.shyd.healthcare.service.user.UserService;
+import com.shyd.healthcare.utils.jwt.JwtTokenFilter;
+import com.shyd.healthcare.utils.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CustomUserDetailsService customUserDetailsService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v1/user/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/post").authenticated()
+                .antMatchers(HttpMethod.POST, "/api/v1/comment").authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(new JwtTokenFilter(jwtTokenProvider, customUserDetailsService),
+                        UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
