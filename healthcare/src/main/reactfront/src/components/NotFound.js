d112be68bb3d563109e6def61efcb40f93e4b0c3
JoSihun <kepler452k@naver.com>
2023-04-13 01:26:35 +0900

    feat(NotFound.js): add page for 404 error

--- healthcare/src/main/reactfront/src/components/NotFound.js
+++ healthcare/src/main/reactfront/src/components/NotFound.js
@@ -1,11 +1,22 @@
 import React from "react";
 
-const NotFound = () => {
+export default function NotFound() {
     return (
-        <div>
-            404 Error
-        </div>
+        <container fluid>
+            <div className="d-flex justify-content-center" style={{ minHeight: "50vh" }}>
+                <div className="align-self-center">
+                    <div style={{ color: "red" }}>
+                        <h1><strong>
+                            [404 ERROR] Page NotFound
+                        </strong></h1>
+                    </div>
+                    <div style={{ color: "black" }}>
+                        <h1><strong><i>
+                            페이지를 찾을 수 없습니다.<br/>
+                        </i></strong></h1>
+                    </div>
+                </div>
+            </div>
+        </container>
     );
 }
-
-export default NotFound;
