8f4897f373963e74a06c82cd640aa1b5addfbd4e
JoSihun <kepler452k@naver.com>
2023-04-13 01:14:18 +0900

    fix(UserRequestDto.java): fix anntaion for @Builder error

--- healthcare/src/main/java/com/shyd/healthcare/dto/user/UserRequestDto.java
+++ healthcare/src/main/java/com/shyd/healthcare/dto/user/UserRequestDto.java
@@ -4,15 +4,22 @@ import com.shyd.healthcare.domain.user.Role;
 import com.shyd.healthcare.domain.user.User;
 import lombok.*;
 
-@Data
-@Builder
+@Getter
+@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class UserRequestDto {
     private Role role;
     private String email;
     private String username;
     private String password;
+
+    @Builder
+    public UserRequestDto(String email, String username, String password, Role role) {
+        this.role = role;
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
 
     public User toEntity() {
         return User.builder()
