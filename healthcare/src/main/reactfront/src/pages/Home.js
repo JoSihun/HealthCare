b9970dad4829c93a57acc2ee163ea3a0fbf0f0c1
JoSihun <kepler452k@naver.com>
2023-04-16 15:57:42 +0900

    feat(Home.js): implement home design

--- healthcare/src/main/reactfront/src/pages/Home.js
+++ healthcare/src/main/reactfront/src/pages/Home.js
@@ -1,33 +1,108 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Container from "react-bootstrap/Container";
+import { Card, Col, Row } from "react-bootstrap";
+import CardImage from "../assets/images/bg_black.jpg";
+import BackGroundImage from "../assets/images/bg_home.jpg";
 
-const TestApi = () => {
-    const [ testString, setTestString ] = useState('Basic Value');
-
+const TestHomeAPI = (props) => {
+    const [testData, setTestData] = useState("Test Data Before");
     useEffect(() => {
-        axios.get("/home")
-        .then((response) => { setTestString(response.data) })
-        .catch((error) => { console.log(error) });
+        const callAPI = async () => {
+            axios.get(`/home`)
+            .then((response) => {
+                setTestData(response.data);
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
 
+        callAPI();
     }, []);
 
+    return <h1>{testData}</h1>
+}
+
+const CardItem = (props) => {
     return (
-        <>
-            <h1>{ testString }</h1><hr/>
-        </>
+        <Card>
+            <Card.Img variant="top" src={CardImage} />
+            <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of the card's content.
+                    <TestHomeAPI />
+                </Card.Text>
+            </Card.Body>
+        </Card>
     );
 }
 
-const Home = () => {
-    return (
-        <>
-            <Container fluid className="min-vh-100">
-                <h1>This is Home.</h1><hr/>
-                <TestApi />
-            </Container>
-        </>
-    );
-}
+export default function Home() {
 
-export default Home;
+
+    return (
+        <Container fluid className="min-vh-100 bg-white text-black"
+            style={{ backgroundImage: `url(${BackGroundImage})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "top center",
+                    backgroundSize: "cover",
+                    backgroundAttachment: "fixed" }}>
+
+            <Row className="justify-content-center py-4" style={{ minHeight: "25vh" }}>
+                <Col className="col-md-3">
+                    <CardItem />
+                </Col>
+                <Col className="col-md-3">
+                    <CardItem />
+                </Col>
+                <Col className="col-md-3">
+                    <CardItem />
+                </Col>
+            </Row>
+
+            <Row className="justify-content-center pb-4" style={{ minHeight: "25vh" }}>
+                <Col className="col-md-3">
+                    <CardItem />
+                </Col>
+                <Col className="col-md-3">
+                    <CardItem />
+                </Col>
+                <Col className="col-md-3">
+                    <CardItem />
+                </Col>
+            </Row>
+
+            <Row className="justify-content-center pb-4" style={{ minHeight: "25vh" }}>
+                <Col className="col-md-3">
+                    <CardItem />
+                </Col>
+                <Col className="col-md-3">
+                    <CardItem />
+                </Col>
+                <Col className="col-md-3">
+                    <CardItem />
+                </Col>
+                <Col className="col-md-3">
+                    <CardItem />
+                </Col>
+            </Row>
+
+            <Row className="justify-content-center pb-4" style={{ minHeight: "25vh" }}>
+                <Col className="col-md-3">
+                    <CardItem />
+                </Col>
+                <Col className="col-md-3">
+                    <CardItem />
+                </Col>
+                <Col className="col-md-3">
+                    <CardItem />
+                </Col>
+                <Col className="col-md-3">
+                    <CardItem />
+                </Col>
+            </Row>
+            
+        </Container>
+    )
+}
