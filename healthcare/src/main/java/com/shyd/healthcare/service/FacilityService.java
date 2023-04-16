a225d1409ae005bd6c0fda3fcb004909a6f1c9a6
jyd12190@gmail.com <jyd12190@gmail.com>
2023-04-16 16:08:44 +0900

    feat(FacilityService.java): implement functions of crud system implement function of save() implement function of update() implement function of delete()

--- healthcare/src/main/java/com/shyd/healthcare/service/FacilityService.java
+++ healthcare/src/main/java/com/shyd/healthcare/service/FacilityService.java
@@ -1,7 +1,12 @@
 package com.shyd.healthcare.service;
 
 import com.shyd.healthcare.domain.Facility;
+import com.shyd.healthcare.domain.Post;
 import com.shyd.healthcare.dto.facility.FacilityResponseDto;
+import com.shyd.healthcare.dto.facility.FacilitySaveRequestDto;
+import com.shyd.healthcare.dto.facility.FacilityUpdateRequestDto;
+import com.shyd.healthcare.dto.post.PostSaveRequestDto;
+import com.shyd.healthcare.dto.post.PostUpdateRequestDto;
 import com.shyd.healthcare.repository.FacilityRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Sort;
@@ -31,5 +36,28 @@ public class FacilityService {
         List<Facility> facilityList = this.facilityRepository.findAll(sort);
         return facilityList.stream().map(FacilityResponseDto::new).collect(Collectors.toList());
     }
+    
+    //추가기능 수정기능, 삭제기능 구현필요
+    /** Facility 저장 */
+    @Transactional
+    public Long save(final FacilitySaveRequestDto requestDto) {
+        return this.facilityRepository.save(requestDto.toEntity()).getId();
+    }
 
+    /** Facility 수정 */
+    @Transactional
+    public Long update(final Long id, final FacilityUpdateRequestDto requestDto) {
+        Facility entity = this.facilityRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. id = " + id));
+        return entity.update(requestDto);
+    }
+
+    /** Facility 삭제 */
+    @Transactional
+    public Long delete(final Long id) {
+        Facility entity = this.facilityRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. id = " + id));
+        this.facilityRepository.delete(entity);
+        return id;
+    }
 }
