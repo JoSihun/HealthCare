7bb760b15c61bcf260f1a00b9812d2aaaea6b5b6
JoSihun <kepler452k@naver.com>
2023-04-18 15:19:02 +0900

    feat(Post.java): connect Entity Table Post with User add contact field in User.java connect Entity Table Post with User

--- healthcare/src/main/java/com/shyd/healthcare/domain/user/User.java
+++ healthcare/src/main/java/com/shyd/healthcare/domain/user/User.java
@@ -1,11 +1,13 @@
 package com.shyd.healthcare.domain.user;
 
 import com.shyd.healthcare.domain.BaseTime;
+import com.shyd.healthcare.domain.support.board.Post;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.List;
 
 @NoArgsConstructor
 @Getter
@@ -17,16 +19,22 @@ public class User extends BaseTime {
     @Column(length = 50, nullable = false, unique = true)
     private String email;
     @Column(length = 50, nullable = false, unique = true)
+    private String contact;
+    @Column(length = 50, nullable = false, unique = true)
     private String username;
     @Column(length = 100, nullable = false)
     private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
 
+    @OneToMany(mappedBy = "author", cascade = CascadeType.REMOVE)
+    private List<Post> posts;
+
     @Builder
-    public User(String email, String username, String password, Role role) {
+    public User(String email, String contact, String username, String password, Role role) {
         this.role = role;
         this.email = email;
+        this.contact = contact;
         this.username = username;
         this.password = password;
     }
