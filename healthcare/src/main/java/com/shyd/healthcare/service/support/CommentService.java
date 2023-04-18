2f7f39697a14ead0bf8b653db1bc251a574200c8
JoSihun <kepler452k@naver.com>
2023-04-18 16:14:22 +0900

    fix(CommentService.java): fix function save() to get user information from token jwtTokenProvider is used to get userId userRepository is used to get user for setAuthor()

--- healthcare/src/main/java/com/shyd/healthcare/service/support/CommentService.java
+++ healthcare/src/main/java/com/shyd/healthcare/service/support/CommentService.java
@@ -2,11 +2,14 @@ package com.shyd.healthcare.service.support;
 
 import com.shyd.healthcare.domain.support.board.Comment;
 import com.shyd.healthcare.domain.support.board.Post;
+import com.shyd.healthcare.domain.user.User;
 import com.shyd.healthcare.dto.support.comment.CommentResponseDto;
 import com.shyd.healthcare.dto.support.comment.CommentSaveRequestDto;
 import com.shyd.healthcare.dto.support.comment.CommentUpdateRequestDto;
 import com.shyd.healthcare.repository.support.CommentRepository;
 import com.shyd.healthcare.repository.support.PostRepository;
+import com.shyd.healthcare.repository.user.UserRepository;
+import com.shyd.healthcare.utils.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,8 +20,10 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @Service
 public class CommentService {
+    private final UserRepository userRepository;
     private final PostRepository postRepository;
     private final CommentRepository commentRepository;
+    private final JwtTokenProvider jwtTokenProvider;
 
     /** 댓글 조회 */
     @Transactional
@@ -31,10 +36,17 @@ public class CommentService {
 
     /** 댓글 생성 */
     @Transactional
-    public Long save(final Long postId, final CommentSaveRequestDto requestDto) {
+    public Long save(String token, final Long postId, final CommentSaveRequestDto requestDto) {
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+        }
+        Long userId = this.jwtTokenProvider.getUserIdFromToken(token);
+        User userEntity = this.userRepository.findById(userId).orElseThrow(
+                () -> new IllegalArgumentException("해당 유저를 찾을 수 없습니다. user_id = " + userId));
         Post postEntity = this.postRepository.findById(postId).orElseThrow(
                 () -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. post_id = " + postId));
         requestDto.setPost(postEntity);
+        requestDto.setAuthor(userEntity);
         return this.commentRepository.save(requestDto.toEntity()).getId();
     }
 
