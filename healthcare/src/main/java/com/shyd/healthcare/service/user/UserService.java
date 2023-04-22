2564b3aaa711502d650b30ec0fa5acb1f34e2751
JoSihun <kepler452k@naver.com>
2023-04-22 16:10:58 +0900

    feat(UserService.java): add basic functions like CRUD System add function findById() in UserService.java add function findAll() in UserService.java add function create() in UserService.java add function update() in UserService.java add function delete() in UserService.java add function existsUsername() in UserService.java add function updatePassword() in UserService.java
    refactor move function signin() to AuthService.java
    refactor move function signup() to AuthService.java
    refactor remove useless comment in UserService.java
    refactor remove useless function findByEmail() in UserService.java
    refactor remove useless function findByUsername() in UserService.java
    refactor remove useless function checkEmail() in UserService.java
    refactor remove useless function checkUsername() in UserService.java

--- healthcare/src/main/java/com/shyd/healthcare/service/user/UserService.java
+++ healthcare/src/main/java/com/shyd/healthcare/service/user/UserService.java
@@ -4,90 +4,73 @@ import com.shyd.healthcare.domain.user.Role;
 import com.shyd.healthcare.domain.user.User;
 import com.shyd.healthcare.dto.user.UserRequestDto;
 import com.shyd.healthcare.repository.user.UserRepository;
-import com.shyd.healthcare.utils.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import com.shyd.healthcare.dto.user.UserResponseDto;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
-    private final JwtTokenProvider jwtTokenProvider;
 
-    /** 회원가입 */
+    /** Usernamee 중복체크 */
     @Transactional
-    public void signup(UserRequestDto requestDto) {
+    public Boolean existsUsername(String username) {
+        return userRepository.existsByUsername(username);
+    }
+    
+    /** User Password 변경 */
+    @Transactional
+    public void updatePassword(Long id, String password) {
+        User user = this.userRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 유저를 찾을 수 없습니다. user_id = " + id));
+        user.updatePassword(passwordEncoder.encode(password));
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /** User 조회 */
+    @Transactional
+    public UserResponseDto findById(Long id) {
+        User user = this.userRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 유저를 찾을 수 없습니다. user_id = " + id));
+        return new UserResponseDto(user);
+    }
+
+    /** User 목록조회 */
+    @Transactional
+    public List<UserResponseDto> findAll() {
+        List<User> users = this.userRepository.findAll();
+        return users.stream().map(UserResponseDto::new).collect(Collectors.toList());
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /** User 삽입 */
+    @Transactional
+    public void create(UserRequestDto requestDto) {
         requestDto.setRole(Role.ROLE_USER);
         requestDto.setPassword(passwordEncoder.encode(requestDto.getPassword()));
         this.userRepository.save(requestDto.toEntity());
     }
 
-    /** 로그인 */
+    /** User 수정 */
     @Transactional
-    public UserResponseDto signin(UserRequestDto requestDto) {
-        User entity = this.userRepository.findByUsername(requestDto.getUsername()).orElseThrow(
-                () -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다. username = " + requestDto.getUsername()));
-        if (!passwordEncoder.matches(requestDto.getPassword(), entity.getPassword())) {
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다. username = " + requestDto.getUsername());
-        }
-//        String token = this.jwtTokenProvider.generateTokenByUsername(new UsernamePasswordAuthenticationToken(entity, null));
-        String token = this.jwtTokenProvider.generateToken(new UsernamePasswordAuthenticationToken(entity, null));
-        UserResponseDto responseDto = new UserResponseDto(entity);
-        responseDto.setAccessToken(token);
-        return responseDto;
+    public void update(Long id, UserRequestDto requestDto) {
+        User user = this.userRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 유저를 찾을 수 없습니다. user_id = " + id));
+        user.update(requestDto);
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /** User 삭제 */
     @Transactional
-    public UserResponseDto findByEmail(String email) {
-        User entity = this.userRepository.findByEmail(email).orElseThrow(
-                () -> new UsernameNotFoundException("해당 유저의 email 을 찾을 수 없습니다. email = " + email));
-        return new UserResponseDto(entity);
+    public void delete(Long id) {
+        User user = this.userRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 유저를 찾을 수 없습니다. user_id = " + id));
+        this.userRepository.delete(user);
     }
-
-    @Transactional
-    public UserResponseDto findByUsername(String username) {
-        User entity = this.userRepository.findByUsername(username).orElseThrow(
-                () -> new UsernameNotFoundException("해당 유저의 username 을 찾을 수 없습니다. email = " + username));
-        return new UserResponseDto(entity);
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    private Boolean checkEmail(String email) {
-        return userRepository.existsByEmail(email);
-    }
-
-    private Boolean checkUsername(String username) {
-        return userRepository.existsByUsername(username);
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//    @Transactional
-//    public Long create(UserRequestDto requestDto) {
-//        requestDto.setPassword(passwordEncoder.encode(requestDto.getPassword()));
-//        return this.userRepository.save(requestDto.toEntity()).getId();
-//    }
-//
-//    @Transactional
-//    public Long update(final Long id, final UserRequestDto requestDto) {
-//        User entity = this.userRepository.findById(id).orElseThrow(
-//                () -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다. user_id = " + id));
-//        entity.update(requestDto);
-//    }
-//
-//    @Transactional
-//    public void delete(final Long id) {
-//        User entity = this.userRepository.findById(id).orElseThrow(
-//                () -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다. user_id = " + id));
-//        this.userRepository.delete(entity);
-//    }
 }
