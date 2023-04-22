ce0ef85a1c2b3c538854a67af83c858bfc8777f1
JoSihun <kepler452k@naver.com>
2023-04-22 15:41:06 +0900

    feat(CustomUserDetailsService.java): implement functions of CustomUserDetailsService for WebSecurity

--- healthcare/src/main/java/com/shyd/healthcare/config/CustomUserDetailsService.java
+++ healthcare/src/main/java/com/shyd/healthcare/config/CustomUserDetailsService.java
@@ -1,6 +1,6 @@
-package com.shyd.healthcare.service.user;
+package com.shyd.healthcare.config;
 
-import com.shyd.healthcare.domain.user.CustomUserDetails;
+import com.shyd.healthcare.config.CustomUserDetails;
 import com.shyd.healthcare.domain.user.User;
 import com.shyd.healthcare.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +18,18 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username).orElseThrow(
                 () -> new UsernameNotFoundException("해당 유저가 존재하지 않습니다. username = " + username));
+        return new CustomUserDetails(user);
+    }
+
+    public UserDetails loadUserById(Long userId) throws IllegalArgumentException {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new IllegalArgumentException("해당 유저가 존재하지 않습니다. user_id = " + userId));
+        return new CustomUserDetails(user);
+    }
+
+    public UserDetails loadUserByEmail(String email) throws IllegalArgumentException {
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new IllegalArgumentException("해당 유저가 존재하지 않습니다. email = " + email));
         return new CustomUserDetails(user);
     }
 }
