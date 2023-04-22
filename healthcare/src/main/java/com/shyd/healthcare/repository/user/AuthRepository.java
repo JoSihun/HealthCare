632bb8bc4c05e828baf906c9090019936d6e84db
JoSihun <kepler452k@naver.com>
2023-04-22 15:32:10 +0900

    feat(AuthRepository.java): add basic function find and exists in AuthRepository.java

--- healthcare/src/main/java/com/shyd/healthcare/repository/user/AuthRepository.java
+++ healthcare/src/main/java/com/shyd/healthcare/repository/user/AuthRepository.java
@@ -1,10 +1,19 @@
 package com.shyd.healthcare.repository.user;
 
 import com.shyd.healthcare.domain.user.Auth;
+import com.shyd.healthcare.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import javax.swing.text.html.Option;
+import java.time.LocalDateTime;
 import java.util.Optional;
 
 public interface AuthRepository extends JpaRepository<Auth, Long> {
+    Optional<Auth> findByUser(User user);
     Optional<Auth> findByAccessToken(String accessToken);
+    Optional<Auth> findByRefreshToken(String refreshToken);
+
+    Boolean existsByUser(User user);
+    Boolean existsByAccessToken(String accessToken);
+    Boolean existsByRefreshToken(String refreshToken);
 }
