582b21a97fdbea6a8598ad206ea03f4929a5df28
jyd12190@gmail.com <jyd12190@gmail.com>
2023-04-26 17:24:26 +0900

    feat(StaffService.java): add functions of basic CRUD System add function save(), update(), delete() in StaffService.java add function findAllAsc(), findAllDesc() in StaffService.java

---
+++ healthcare/src/main/java/com/shyd/healthcare/service/introduce/StaffService.java
@@ -0,0 +1,62 @@
+package com.shyd.healthcare.service.introduce;
+
+import com.shyd.healthcare.domain.introduce.Facility;
+import com.shyd.healthcare.domain.introduce.Staff;
+import com.shyd.healthcare.dto.introduce.facility.FacilitySaveRequestDto;
+import com.shyd.healthcare.dto.introduce.facility.FacilityUpdateRequestDto;
+import com.shyd.healthcare.dto.introduce.staff.StaffResponseDto;
+import com.shyd.healthcare.dto.introduce.staff.StaffSaveRequestDto;
+import com.shyd.healthcare.dto.introduce.staff.StaffUpdateRequestDto;
+import com.shyd.healthcare.repository.introduce.StaffRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class StaffService {
+    private final StaffRepository staffRepository;
+
+    /** 모든 자료 불러오기 오름차순 */
+    @Transactional
+    public List<StaffResponseDto> findAllAsc() {
+        Sort sort = Sort.by(Sort.Direction.ASC, "id");
+        List<Staff> staffList = this.staffRepository.findAll(sort);
+        return staffList.stream().map(StaffResponseDto::new).collect(Collectors.toList());
+    }
+
+    /** 모든 자료 불러오기 내림차순 */
+    @Transactional
+    public List<StaffResponseDto> findAllDesc() {
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        List<Staff> staffList = this.staffRepository.findAll(sort);
+        return staffList.stream().map(StaffResponseDto::new).collect(Collectors.toList());
+    }
+
+    /** Staff 저장 */
+    @Transactional
+    public Long save(final StaffSaveRequestDto requestDto) {
+        return this.staffRepository.save(requestDto.toEntity()).getId();
+    }
+
+    /** Staff 수정 */
+    @Transactional
+    public Long update(final Long id, final StaffUpdateRequestDto requestDto) {
+        Staff entity = this.staffRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 정보는 존재하지 않습니다. id = " + id));
+        return entity.update(requestDto);
+    }
+
+    /** Staff 삭제 */
+    @Transactional
+    public Long delete(final Long id) {
+        Staff entity = this.staffRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 정보는 존재하지 않습니다. id = " + id));
+        this.staffRepository.delete(entity);
+        return id;
+    }
+}
