0e2c9993075c580f09c54fa90c2478e8a753b543
jyd12190@gmail.com <jyd12190@gmail.com>
2023-04-29 20:12:35 +0900

    feat(StaffSaveRequestDto.java): connect staff with user

--- healthcare/src/main/java/com/shyd/healthcare/dto/introduce/staff/StaffSaveRequestDto.java
+++ healthcare/src/main/java/com/shyd/healthcare/dto/introduce/staff/StaffSaveRequestDto.java
@@ -1,23 +1,30 @@
 package com.shyd.healthcare.dto.introduce.staff;
 
 import com.shyd.healthcare.domain.introduce.Staff;
+import com.shyd.healthcare.domain.user.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class StaffSaveRequestDto {
+    private User user;
     private String staffRole;
 
+
     @Builder
-    public StaffSaveRequestDto(String staffRole) {
+    public StaffSaveRequestDto(User user, String staffRole) {
+        this.user = user;
         this.staffRole = staffRole;
     }
 
     public Staff toEntity() {
         return Staff.builder()
-                .staffRole(staffRole)
+                .user(this.user)
+                .staffRole(this.staffRole)
                 .build();
     }
 }
