2e73b552aa6bd7ce1dc961e58bfc298cf58d3990
jyd12190@gmail.com <jyd12190@gmail.com>
2023-04-29 20:13:59 +0900

    fix(StaffRepository.java):  import problem solved

--- healthcare/src/main/java/com/shyd/healthcare/repository/introduce/StaffRepository.java
+++ healthcare/src/main/java/com/shyd/healthcare/repository/introduce/StaffRepository.java
@@ -1,7 +1,11 @@
 package com.shyd.healthcare.repository.introduce;
 
 import com.shyd.healthcare.domain.introduce.Staff;
+import com.shyd.healthcare.domain.support.board.Post;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface StaffRepository extends JpaRepository<Staff, Long> {
 }
