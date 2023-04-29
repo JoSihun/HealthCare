bef3211cc61e690c3ceabbf2a248ea4cb6370000
jyd12190@gmail.com <jyd12190@gmail.com>
2023-04-29 20:15:35 +0900

    feat(UserRepository.java): implement function of findAllByUsernameContaining for searching username in staff.js

--- healthcare/src/main/java/com/shyd/healthcare/repository/user/UserRepository.java
+++ healthcare/src/main/java/com/shyd/healthcare/repository/user/UserRepository.java
@@ -3,11 +3,13 @@ package com.shyd.healthcare.repository.user;
 import com.shyd.healthcare.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
     Optional<User> findByEmail(String email);
+    List<User> findAllByUsernameContaining(String username);
     Boolean existsByUsername(String username);
     Boolean existsByEmail(String email);
 }
