b2da9a204d6090ec071d6256742358e2a9f04453
JoSihun <kepler452k@naver.com>
2023-05-04 15:22:46 +0900

    test(FoodServiceTest.java): fix test function as service function recommendByBmi() in FoodService.java changed fix function name recommend() to recommendByBmiTest() in FoodServiceTest.java fix function recommendByBmiTest() as service function recommendByBmi() in FoodService.java changed

--- healthcare/src/test/java/com/shyd/healthcare/service/management/FoodServiceTest.java
+++ healthcare/src/test/java/com/shyd/healthcare/service/management/FoodServiceTest.java
@@ -1,6 +1,7 @@
 package com.shyd.healthcare.service.management;
 
 import com.shyd.healthcare.domain.management.BMI;
+import com.shyd.healthcare.domain.user.User;
 import com.shyd.healthcare.dto.management.bmi.BMIResponseDto;
 import com.shyd.healthcare.dto.management.food.FoodResponseDto;
 import com.shyd.healthcare.repository.management.FoodRepository;
@@ -11,30 +12,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
+
 @SpringBootTest
 class FoodServiceTest {
-    @Autowired
-    private BMIService bmiService;
     @Autowired
     private FoodService foodService;
 
     @Test
-    @DisplayName("Top 5 식단 추천 기능")
-    void recommend() {
-        BMIResponseDto bmiResponseDto = this.bmiService.findById(488L);
-        List<FoodResponseDto> responseDtos = this.foodService.recommend(bmiResponseDto.getBasalMetabolicRate(), 9);
-        for (FoodResponseDto responseDto : responseDtos) {
-            System.out.print("음식명: " + responseDto.getName());
-            System.out.print(" | 중량: " + responseDto.getWeight());
-            System.out.print(" | 열량: " + responseDto.getCalories());
-            System.out.print(" | 탄수화물: " + responseDto.getCarbohydrates());
-            System.out.print(" | 단백질: " + responseDto.getProteins());
-            System.out.print(" | 지방: " + responseDto.getFats());
-            System.out.print(" | 당류: " + responseDto.getSugars());
-            System.out.print(" | 나트륨: " + responseDto.getSodium());
-            System.out.print(" | 콜레스테롤: " + responseDto.getCholesterol());
-            System.out.print(" | 포화지방산: " + responseDto.getSaturatedFattyAcids());
-            System.out.println(" | 트랜스지방산: " + responseDto.getTransFattyAcids());
+    @DisplayName("Top 9 식단 추천 기능")
+    void recommendByBmiTest() {
+        List<FoodResponseDto> foodResponseDtoList = this.foodService.recommendByBmi(488L, 9);
+        for (FoodResponseDto foodResponseDto : foodResponseDtoList) {
+            System.out.print("음식명: " + foodResponseDto.getName());
+            System.out.print(" | 중량: " + foodResponseDto.getWeight());
+            System.out.print(" | 열량: " + foodResponseDto.getCalories());
+            System.out.print(" | 탄수화물: " + foodResponseDto.getCarbohydrates());
+            System.out.print(" | 단백질: " + foodResponseDto.getProteins());
+            System.out.print(" | 지방: " + foodResponseDto.getFats());
+            System.out.print(" | 당류: " + foodResponseDto.getSugars());
+            System.out.print(" | 나트륨: " + foodResponseDto.getSodium());
+            System.out.print(" | 콜레스테롤: " + foodResponseDto.getCholesterol());
+            System.out.print(" | 포화지방산: " + foodResponseDto.getSaturatedFattyAcids());
+            System.out.println(" | 트랜스지방산: " + foodResponseDto.getTransFattyAcids());
         }
     }
 }
