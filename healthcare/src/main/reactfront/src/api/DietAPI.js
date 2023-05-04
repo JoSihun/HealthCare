ec3c2aa3f5fca7635496299b7e5502307014cc47
JoSihun <kepler452k@naver.com>
2023-05-04 15:05:34 +0900

    feat(DietAPI.js): implement functions of Diet add function fetchDiet() in DietAPI.js add function fetchDiets() in DietAPI.js add function fetchRecommendFoods() in DietAPI.js add function createDiet() in DietAPI.js add function updateDiet() in DietAPI.js add function deleteDiet() in DietAPI.js

--- healthcare/src/main/reactfront/src/api/DietAPI.js
+++ healthcare/src/main/reactfront/src/api/DietAPI.js
@@ -12,3 +12,41 @@ export const DietApi = axios.create({
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
+/** 식단 조회 API */
+export const fetchDiet = async ({ id }) => {
+    const response = await DietApi.get(`/api/v1/diet/${id}`);
+    return response.data;
+}
+
+/** 식단 목록조회 API - Page */
+export const fetchDiets = async (page, size) => {
+    const queryString = `page=${page - 1}&size=${size}`;
+    const response = await DietApi.get(`/api/v1/diet/list?${queryString}`);
+    return response.data;
+}
+
+/** 추천식단 조회 - List */
+export const fetchRecommendFoods = async (bmiId) => {
+    const queryString = `bmiId=${bmiId}`;
+    const response = await DietApi.get(`/api/v1/diet/recommend?${queryString}`);
+    return response.data;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/** 식단 삽입 API */
+export const createDiet = async (data) => {
+    const response = await DietApi.post(`/api/v1/diet`, data);
+    return response.data;
+}
+
+/** 식단 수정 API */
+export const updateDiet = async ({ id, data }) => {
+    const response = await DietApi.put(`/api/v1/diet/${id}`, data);
+    return response.data;
+}
+
+/** 식단 삭제 API */
+export const deleteDiet = async (id) => {
+    const response = await DietApi.delete(`/api/v1/diet/${id}`);
+    return response.data;
+}
