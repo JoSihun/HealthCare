ebe197e8a5c7bd669aa72be993b0fae4c77e4c51
JoSihun <kepler452k@naver.com>
2023-05-06 15:24:08 +0900

    refactor(Auth.java): refactor simplify by using lobok plugins

--- healthcare/src/main/java/com/shyd/healthcare/domain/user/Auth.java
+++ healthcare/src/main/java/com/shyd/healthcare/domain/user/Auth.java
@@ -4,43 +4,30 @@ import com.shyd.healthcare.domain.BaseTime;
 import lombok.*;
 
 import javax.persistence.*;
-import java.time.LocalDateTime;
 
-@NoArgsConstructor
 @Getter
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Auth extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @Column(nullable = false)
     private String tokenType;
-
     @Column(nullable = false)
     private String accessToken;
-
     @Column(nullable = false)
     private String refreshToken;
 
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
     private User user;
-
-    @Builder
-    public Auth(User user, String tokenType, String accessToken, String refreshToken) {
-        this.user = user;
-        this.tokenType = tokenType;
-        this.accessToken = accessToken;
-        this.refreshToken = refreshToken;
-    }
 
     public void updateAccessToken(String accessToken) {
         this.accessToken = accessToken;
     }
-
     public void updateRefreshToken(String refreshToken) {
         this.refreshToken = refreshToken;
     }
-
 }
