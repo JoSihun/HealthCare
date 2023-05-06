303828c38569aba5b2f8b164208ab2f3a01314d0
JoSihun <kepler452k@naver.com>
2023-05-06 19:26:11 +0900

    feat(UserResponseDto.java): add field name and birthday add field name and birthday as added in User Entity remove field role which is useless as a response

--- healthcare/src/main/java/com/shyd/healthcare/dto/user/UserResponseDto.java
+++ healthcare/src/main/java/com/shyd/healthcare/dto/user/UserResponseDto.java
@@ -1,22 +1,27 @@
 package com.shyd.healthcare.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.shyd.healthcare.domain.user.User;
 import lombok.*;
+
+import java.time.LocalDate;
 
 @Getter
 @Setter
 public class UserResponseDto {
     private Long id;
-    private String role;
+    private String name;
     private String email;
     private String contact;
     private String username;
+    private String birthday;
 
     public UserResponseDto(User entity) {
         this.id = entity.getId();
+        this.name = entity.getName();
         this.email = entity.getEmail();
         this.contact = entity.getContact();
         this.username = entity.getUsername();
-        this.role = entity.getRole().name();
+        this.birthday = entity.getBirthday().toString();
     }
 }
