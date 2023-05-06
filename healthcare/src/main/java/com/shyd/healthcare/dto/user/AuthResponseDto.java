aa31c3d841793164d245268af991184cb2044ecc
JoSihun <kepler452k@naver.com>
2023-05-06 17:09:11 +0900

    refactor(AuthResponseDto.java): refactor remove useless annotation

--- healthcare/src/main/java/com/shyd/healthcare/dto/user/AuthResponseDto.java
+++ healthcare/src/main/java/com/shyd/healthcare/dto/user/AuthResponseDto.java
@@ -3,18 +3,15 @@ package com.shyd.healthcare.dto.user;
 import com.shyd.healthcare.domain.user.Auth;
 import lombok.*;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuthResponseDto {
+    private String tokenType;
     private String accessToken;
     private String refreshToken;
-    private String tokenType = "Bearer";
 
-    @Builder
     public AuthResponseDto(Auth entity) {
         this.tokenType = entity.getTokenType();
         this.accessToken = entity.getAccessToken();
