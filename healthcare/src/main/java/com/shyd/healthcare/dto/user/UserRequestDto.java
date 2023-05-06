a68185f49d1c9db564262ea77a528d8db34758eb
JoSihun <kepler452k@naver.com>
2023-05-06 19:24:48 +0900

    feat(UserRequestDto.java): add field name and birthday add field name and birthday as added in User Entity refactor field role

--- healthcare/src/main/java/com/shyd/healthcare/dto/user/UserRequestDto.java
+++ healthcare/src/main/java/com/shyd/healthcare/dto/user/UserRequestDto.java
@@ -4,24 +4,33 @@ import com.shyd.healthcare.domain.user.Role;
 import com.shyd.healthcare.domain.user.User;
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserRequestDto {
-    private Role role = Role.ROLE_USER;
+    // PERSONAL INFO
+    private String name;
     private String email;
     private String contact;
+    private String birthday;
+
+    // ACCOUNT INFO
     private String username;
     private String password;
+    private Role role;
 
     public User toEntity() {
         return User.builder()
                 .role(this.role)
+                .name(this.name)
                 .email(this.email)
                 .contact(this.contact)
                 .username(this.username)
                 .password(this.password)
+                .birthday(LocalDate.parse(this.birthday))
                 .build();
     }
 }
