e938f4b7a80c54e6145d10ef65db10cfb945c1d7
JoSihun <kepler452k@naver.com>
2023-05-08 13:39:37 +0900

    feat(JwtTokenFilter.java): add function of validate refresh token rename function getTokenFromRequest() to getAccessTokenFromRequest() add function getRefreshTokenFromRequest() add condition that validates refresh token in function doFilterInternal()

--- healthcare/src/main/java/com/shyd/healthcare/config/JwtTokenFilter.java
+++ healthcare/src/main/java/com/shyd/healthcare/config/JwtTokenFilter.java
@@ -3,7 +3,6 @@ package com.shyd.healthcare.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -14,6 +13,50 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider jwtTokenProvider;
+    private final CustomUserDetailsService customUserDetailsService;
+    private static final String BEARER_PREFIX = "Bearer ";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String accessToken = getAccessTokenFromRequest(request);
+        String refreshToken = getRefreshTokenFromRequest(request);
+        if (accessToken != null && this.jwtTokenProvider.validateToken(accessToken)) {
+            if (refreshToken != null && this.jwtTokenProvider.validateToken(refreshToken)) {
+                UsernamePasswordAuthenticationToken authentication = getAuthenticationFromToken(accessToken);
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String getAccessTokenFromRequest(HttpServletRequest request) {
+        String bearerToken = request.getHeader("Authorization");
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(BEARER_PREFIX.length());
+        }
+        return null;
+    }
+
+    private String getRefreshTokenFromRequest(HttpServletRequest request) {
+        String refreshToken = request.getHeader("X-Refresh-Token");
+        if (StringUtils.hasText(refreshToken)) {
+            return refreshToken;
+        }
+        return null;
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthenticationFromToken(String token) {
+        Long userId = this.jwtTokenProvider.getUserIdFromToken(token);
+        CustomUserDetails customUserDetails = (CustomUserDetails) this.customUserDetailsService.loadUserById(userId);
+        return new UsernamePasswordAuthenticationToken(customUserDetails, null, customUserDetails.getAuthorities());
+    }
+}
 
 //public class JwtTokenFilter extends OncePerRequestFilter {
 //    private final JwtTokenProvider jwtTokenProvider;
@@ -60,36 +103,3 @@ import java.io.IOException;
 //        return null;
 //    }
 //}
-
-@Component
-@RequiredArgsConstructor
-public class JwtTokenFilter extends OncePerRequestFilter {
-    private final JwtTokenProvider jwtTokenProvider;
-    private final CustomUserDetailsService customUserDetailsService;
-
-    @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String accessToken = getTokenFromRequest(request);
-        if (accessToken != null && jwtTokenProvider.validateToken(accessToken)) {
-            // RefreshToken 유효성 검사 처리 추가 요망
-            UsernamePasswordAuthenticationToken authentication = getAuthenticationFromToken(accessToken);
-            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
-        filterChain.doFilter(request, response);
-    }
-
-    private String getTokenFromRequest(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
-        }
-        return null;
-    }
-
-    private UsernamePasswordAuthenticationToken getAuthenticationFromToken(String token) {
-        Long userId = jwtTokenProvider.getUserIdFromToken(token);
-        UserDetails userDetails = customUserDetailsService.loadUserById(userId);
-        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-    }
-}
