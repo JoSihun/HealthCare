6d833203ac5484b64fc995b47b5c71e16446b34d
JoSihun <kepler452k@naver.com>
2023-05-08 18:59:39 +0900

    refactor(PostController.java): refactor PostController.java as PostService changed and to look more clearly

--- healthcare/src/main/java/com/shyd/healthcare/controller/support/PostController.java
+++ healthcare/src/main/java/com/shyd/healthcare/controller/support/PostController.java
@@ -1,7 +1,8 @@
 package com.shyd.healthcare.controller.support;
 
-import com.shyd.healthcare.domain.support.board.Category;
-import com.shyd.healthcare.dto.support.post.PostResponseDto;
+import com.shyd.healthcare.domain.support.board.BoardType;
+import com.shyd.healthcare.dto.support.post.PostListResponseDTO;
+import com.shyd.healthcare.dto.support.post.PostResponseDTO;
 import com.shyd.healthcare.service.support.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -10,75 +11,83 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
-@RequiredArgsConstructor
 @RestController
+@RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
 
-    /** GET REQUEST - header: "default" */
+    /** 게시글 조회 API */
     @GetMapping("/api/v1/post/{id}")
-    public PostResponseDto getPost(@PathVariable(value = "id") Long id) {
+    public PostResponseDTO fetchPost(@PathVariable(value = "id") Long id) {
         return this.postService.findById(id);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /** FAQ 게시판 목록조회 API */
     @GetMapping("/api/v1/post/faq-board")
-    public Page<PostResponseDto> readFAQBoard(
-            @PageableDefault(sort = "id", size = 20, direction = Sort.Direction.DESC) Pageable pageable) {
-        return this.postService.findAllByCategoryDesc(Category.FAQ_BOARD.name(), pageable);
+    public Page<PostListResponseDTO> fetchFaqBoard(
+            @PageableDefault(size = 20, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return this.postService.findAllByBoardType(BoardType.FAQ_BOARD, pageable);
     }
 
+    /** Q&A 게시판 목록조회 API */
     @GetMapping("/api/v1/post/qna-board")
-    public Page<PostResponseDto> readQNABoard(
-            @PageableDefault(sort = "id", size = 20, direction = Sort.Direction.DESC) Pageable pageable) {
-        return this.postService.findAllByCategoryDesc(Category.QNA_BOARD.name(), pageable);
+    public Page<PostListResponseDTO> fetchQnaBoard(
+            @PageableDefault(size = 20, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return this.postService.findAllByBoardType(BoardType.QNA_BOARD, pageable);
     }
 
+    /** FREE 게시판 목록조회 API */
     @GetMapping("/api/v1/post/free-board")
-    public Page<PostResponseDto> readFreeBoard(
-            @PageableDefault(sort = "id", size = 20, direction = Sort.Direction.DESC) Pageable pageable) {
-        return this.postService.findAllByCategoryDesc(Category.FREE_BOARD.name(), pageable);
+    public Page<PostListResponseDTO> fetchFreeBoard(
+            @PageableDefault(size = 20, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return this.postService.findAllByBoardType(BoardType.FREE_BOARD, pageable);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    private Page<PostResponseDto> search(String category, String searchValue, String searchFilter, Pageable pageable) {
-        if (searchFilter.equals("Title"))
-            return this.postService.findAllByCategoryAndTitle(category, searchValue, pageable);
-        else if (searchFilter.equals("Content"))
-            return this.postService.findAllByCategoryAndContent(category, searchValue, pageable);
-        else if (searchFilter.equals("Author"))
-            return this.postService.findAllByCategoryAndAuthor(category, searchValue, pageable);
-        else if (searchFilter.equals("TitleContent"))
-            return this.postService.findAllByCategoryAndTitleOrContent(category, searchValue, searchValue, pageable);
-        else if (searchFilter.equals("TitleAuthor"))
-            return this.postService.findAllByCategoryAndTitleOrAuthor(category, searchValue, searchValue, pageable);
-        else if (searchFilter.equals("ContentAuthor"))
-            return this.postService.findAllByCategoryAndContentOrAuthor(category, searchValue, searchValue, pageable);
-        else
-            return this.postService.findAllByCategoryDesc(category, pageable);
+    private Page<PostListResponseDTO> search(BoardType boardType, String searchValue, String searchFilter, Pageable pageable) {
+        switch (searchFilter) {
+            case "Title":
+                return this.postService.findAllByBoardTypeAndTitle(boardType, searchValue, pageable);
+            case "Content":
+                return this.postService.findAllByBoardTypeAndContent(boardType, searchValue, pageable);
+            case "Author":
+                return this.postService.findAllByBoardTypeAndAuthor(boardType, searchValue, pageable);
+            case "TitleContent":
+                return this.postService.findAllByBoardTypeAndTitleOrContent(boardType, searchValue, searchValue, pageable);
+            case "TitleAuthor":
+                return this.postService.findAllByBoardTypeAndTitleOrAuthor(boardType, searchValue, searchValue, pageable);
+            case "ContentAuthor":
+                return this.postService.findAllByBoardTypeAndContentOrAuthor(boardType, searchValue, searchValue, pageable);
+            default:
+                return this.postService.findAllByBoardType(boardType, pageable);
+        }
     }
 
+    /** FAQ 게시판 검색목록조회 API */
     @GetMapping("/api/v1/post/faq-board/search")
-    public Page<PostResponseDto> searchFAQBoard(
+    public Page<PostListResponseDTO> searchFaqBoard(
             @RequestParam(value = "searchValue", required = false) String searchValue,
             @RequestParam(value = "searchFilter", required = false) String searchFilter,
-            @PageableDefault(sort = "id", size = 20, direction = Sort.Direction.DESC) Pageable pageable) {
-        return search(Category.FAQ_BOARD.name(), searchValue, searchFilter, pageable);
+            @PageableDefault(size = 20, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return search(BoardType.FAQ_BOARD, searchValue, searchFilter, pageable);
     }
 
+    /** Q&A 게시판 검색목록조회 API */
     @GetMapping("/api/v1/post/qna-board/search")
-    public Page<PostResponseDto> searchQNABoard(
+    public Page<PostListResponseDTO> searchQnaBoard(
             @RequestParam(value = "searchValue", required = false) String searchValue,
             @RequestParam(value = "searchFilter", required = false) String searchFilter,
-            @PageableDefault(sort = "id", size = 20, direction = Sort.Direction.DESC) Pageable pageable) {
-        return search(Category.QNA_BOARD.name(), searchValue, searchFilter, pageable);
+            @PageableDefault(size = 20, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return search(BoardType.QNA_BOARD, searchValue, searchFilter, pageable);
     }
 
+    /** FREE 게시판 검색목록조회 API */
     @GetMapping("/api/v1/post/free-board/search")
-    public Page<PostResponseDto> searchFreeBoard(
+    public Page<PostListResponseDTO> searchFreeBoard(
             @RequestParam(value = "searchValue", required = false) String searchValue,
             @RequestParam(value = "searchFilter", required = false) String searchFilter,
-            @PageableDefault(sort = "id", size = 20, direction = Sort.Direction.DESC) Pageable pageable) {
-        return search(Category.FREE_BOARD.name(), searchValue, searchFilter, pageable);
+            @PageableDefault(size = 20, sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+        return search(BoardType.FREE_BOARD, searchValue, searchFilter, pageable);
     }
 }
