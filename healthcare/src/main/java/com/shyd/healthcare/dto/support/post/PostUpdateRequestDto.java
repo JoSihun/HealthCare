0372c83991c9644131742523f6a66c3620848a54
JoSihun <kepler452k@naver.com>
2023-05-08 18:42:56 +0900

    refactor(PostUpdateRequestDTO.java): refactor PostUpdateRequestDTO.java refactor import lines refactor constructor by annotation

--- healthcare/src/main/java/com/shyd/healthcare/dto/support/post/PostUpdateRequestDto.java
+++ healthcare/src/main/java/com/shyd/healthcare/dto/support/post/PostUpdateRequestDto.java
@@ -1,25 +1,17 @@
 package com.shyd.healthcare.dto.support.post;
 
 import com.shyd.healthcare.domain.support.board.Post;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Getter
+@Setter
 @NoArgsConstructor
-public class PostUpdateRequestDto {
+@AllArgsConstructor
+public class PostUpdateRequestDTO {
     private String title;
     private String content;
     private Boolean secretYn;
     private Boolean answerYn;
-
-    @Builder
-    public PostUpdateRequestDto(String title, String content, Boolean secretYn, Boolean answerYn) {
-        this.title = title;
-        this.content = content;
-        this.secretYn = secretYn;
-        this.answerYn = answerYn;
-    }
 
     public Post toEntity() {
         return Post.builder()
