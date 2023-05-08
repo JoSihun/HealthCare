40439d16c44dc7e44a07772664d01035ef27a6d1
JoSihun <kepler452k@naver.com>
2023-05-08 20:06:22 +0900

    test(PostServiceTest.java): refactor PostServiceTest.java as PostSaveRequestDTO.java changed

---
+++ healthcare/src/test/java/com/shyd/healthcare/service/support/PostServiceTest.java
@@ -0,0 +1,93 @@
+package com.shyd.healthcare.service.support;
+
+import com.shyd.healthcare.domain.support.board.BoardType;
+import com.shyd.healthcare.dto.support.post.PostSaveRequestDTO;
+import com.shyd.healthcare.dto.user.AuthRequestDto;
+import com.shyd.healthcare.dto.user.AuthResponseDto;
+import com.shyd.healthcare.service.user.AuthService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+class PostServiceTest {
+    @Autowired
+    private AuthService authService;
+    @Autowired
+    private PostService postService;
+
+    @Test
+    void findAllByBoardType() {
+    }
+
+    @Test
+    void testFindAllByBoardType() {
+    }
+
+    @Test
+    void findAllByBoardTypeAndTitle() {
+    }
+
+    @Test
+    void findAllByBoardTypeAndContent() {
+    }
+
+    @Test
+    void findAllByBoardTypeAndAuthor() {
+    }
+
+    @Test
+    void findAllByBoardTypeAndTitleOrContent() {
+    }
+
+    @Test
+    void findAllByBoardTypeAndTitleOrAuthor() {
+    }
+
+    @Test
+    void findAllByBoardTypeAndContentOrAuthor() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    @DisplayName("Test Data 대량 삽입")
+    void createTestData() {
+        AuthRequestDto authRequestDto = new AuthRequestDto("test7218", "test");
+        AuthResponseDto authResponseDto = this.authService.login(authRequestDto);
+
+        for (int i = 0; i < 300; i++) {
+            PostSaveRequestDTO postSaveRequestDTO = new PostSaveRequestDTO();
+            postSaveRequestDTO.setTitle("TestTitle" + (i + 1));
+            postSaveRequestDTO.setContent("TestContent" + (i + 1));
+            postSaveRequestDTO.setSecretYn((i % 2) == 0);                       // true, false, true, false
+            postSaveRequestDTO.setAnswerYn((i % 4 == 0) || (i % 4 == 1));       // true, true, false, false
+
+            postSaveRequestDTO.setBoardType(BoardType.FAQ_BOARD);
+            this.postService.create(authResponseDto.getAccessToken(), postSaveRequestDTO);
+            postSaveRequestDTO.setBoardType(BoardType.QNA_BOARD);
+            this.postService.create(authResponseDto.getAccessToken(), postSaveRequestDTO);
+            postSaveRequestDTO.setBoardType(BoardType.FREE_BOARD);
+            this.postService.create(authResponseDto.getAccessToken(), postSaveRequestDTO);
+        }
+    }
+
+    @Test
+    void create() {
+
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void delete() {
+    }
+}
