5d9023f50e1a2570bffea56f92e63504554b6822
JoSihun <kepler452k@naver.com>
2023-05-08 20:30:56 +0900

    refactor(Comment.java): refactor Comment.java as annotation changed

--- healthcare/src/main/java/com/shyd/healthcare/domain/support/board/Comment.java
+++ healthcare/src/main/java/com/shyd/healthcare/domain/support/board/Comment.java
@@ -3,34 +3,27 @@ package com.shyd.healthcare.domain.support.board;
 import com.shyd.healthcare.domain.BaseTime;
 import com.shyd.healthcare.domain.user.User;
 import com.shyd.healthcare.dto.support.comment.CommentUpdateRequestDto;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 
-@NoArgsConstructor
 @Getter
 @Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Comment extends BaseTime {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(columnDefinition = "LONGTEXT")
     private String content;
-    private Boolean secretYn;
+    private Boolean secretYn = false;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Post post;
     @ManyToOne(fetch = FetchType.LAZY)
     private User author;
-
-    @Builder
-    public Comment(String content, User author, Boolean secretYn, Post post) {
-        this.post = post;
-        this.author = author;
-        this.content = content;
-        this.secretYn = secretYn;
-    }
 
     public Long update(CommentUpdateRequestDto requestDto) {
         this.content = requestDto.getContent();
