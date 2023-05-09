bbb784129eb51a4d1257ecbb74c6a058e044528b
JoSihun <kepler452k@naver.com>
2023-05-09 18:15:17 +0900

    fix(AttachmenService.java): fix and refactor AttachmentService.java refactor annotations remove unused function delete()
    fix function download() return type
    fix function download() as return type changed
    
    rename function saveFiles() to uploadFile()
    fix function uploadFile() return type
    refactor function uploadFile() simplify
    
    rename function deleteFiles() to deleteFile()
    fix function deleteFile() parameter
    fix function deleteFile() as parameter changed
    
    refactor function findAllByPostId()
    rename function findAllByPostId() variable names
    
    rename function save() to create()
    refactor function create() simplify
    
    fix function update() parameter
    fix function update() as parameter changed
    implement function of comparing existing data
    
    refactor function deleteAllByPostId() variable names
    refactor function deleteAllByPostId() as some functions changed

--- healthcare/src/main/java/com/shyd/healthcare/service/support/AttachmentService.java
+++ healthcare/src/main/java/com/shyd/healthcare/service/support/AttachmentService.java
@@ -2,138 +2,127 @@ package com.shyd.healthcare.service.support;
 
 import com.shyd.healthcare.domain.support.board.Attachment;
 import com.shyd.healthcare.domain.support.board.Post;
-import com.shyd.healthcare.dto.support.attachment.AttachmentResponseDto;
-import com.shyd.healthcare.dto.support.attachment.AttachmentSaveRequestDto;
+import com.shyd.healthcare.dto.support.attachment.AttachmentResponseDTO;
+import com.shyd.healthcare.dto.support.attachment.AttachmentRequestDTO;
 import com.shyd.healthcare.repository.support.AttachmentRepository;
 import com.shyd.healthcare.repository.support.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.util.UriUtils;
 
 import javax.transaction.Transactional;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class AttachmentService {
     private final PostRepository postRepository;
     private final AttachmentRepository attachmentRepository;
 
-    /** 단일 첨부파일 삭제 */
+    /** 첨부파일 다운로드 */
     @Transactional
-    public void delete(final Long attachmentId) {
-        Attachment entity = this.attachmentRepository.findById(attachmentId).orElseThrow(
-                () -> new IllegalArgumentException("해당 첨부파일이 존재하지 않습니다. attachment_id = " + attachmentId));
-        this.attachmentRepository.delete(entity);
+    public Resource download(Long id) throws MalformedURLException {
+        Attachment attachment = this.attachmentRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 첨부파일을 찾을 수 없습니다. attachment_id = " + id));
+        return new UrlResource(Paths.get(attachment.getFilePath()).toUri());
     }
 
-    /** 단일 첨부파일 다운로드 API */
-    @Transactional
-    public ResponseEntity<Resource> download(Long attachmentId) throws MalformedURLException {
-        Attachment entity = this.attachmentRepository.findById(attachmentId).orElseThrow(
-                () -> new IllegalArgumentException("해당 첨부파일이 존재하지 않습니다. attachment_id = " + attachmentId));
+    /** 실제파일 저장 */
+    private AttachmentRequestDTO uploadFile(Post post, MultipartFile file) throws IOException {
+        // 저장경로 설정 (Windows: "/", Linux: "\\", Default: "\\")
+        Path basePath = Paths.get(System.getProperty("user.dir"));
+        Path savePath = basePath.getParent().resolve("attachments");
 
-        UrlResource resource = new UrlResource("file:" + entity.getFilePath());
-        String encodedFileName = UriUtils.encode(entity.getFileName(), StandardCharsets.UTF_8);
-        String contentDisposition = "attachment; filename=\"" + encodedFileName + "\"";
-        return ResponseEntity.ok()
-                .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
-                .contentType(MediaType.APPLICATION_OCTET_STREAM)
-                .body(resource);
+        // 파일이름, 파일경로 설정 및 파일 저장
+        String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
+        Path filePath = savePath.resolve(fileName);
+        file.transferTo(filePath);
+
+        // Linux 파일 권한 적용
+        // Runtime.getRuntime().exec("chmod -R 777 " + saveFile);
+
+        // AttachmentSaveRequestDto 생성 및 반환
+        return AttachmentRequestDTO.builder()
+                .fileName(file.getOriginalFilename())
+                .filePath(filePath.toString())
+                .fileSize(file.getSize())
+                .post(post)
+                .build();
     }
 
-    /** 실제 첨부파일 저장 */
-    private void saveFiles(List<MultipartFile> files, Post entity) throws IOException {
-        for (MultipartFile file : files) {
-            // 파일 경로 및 이름 설정 (Windows "/", Linux "\\")
-            File basePath = new File(System.getProperty("user.dir"));
-            String filePath = basePath.getParent() + "/AttachmentFiles";
-            String fileName = UUID.randomUUID() + "_" + file.getOriginalFilename();
-
-            // 파일 저장
-            File saveFile = new File(filePath, fileName);
-            file.transferTo(saveFile);
-
-            // Linux 파일 권한 적용
-            // Runtime.getRuntime().exec("chmod -R 777 " + saveFile);
-
-            // AttachmentSaveRequestDto 생성
-            AttachmentSaveRequestDto requestDto = new AttachmentSaveRequestDto();
-            requestDto.setPost(entity);
-            requestDto.setSize(file.getSize());
-            requestDto.setFileName(file.getOriginalFilename());
-            requestDto.setFilePath(filePath + "/" + fileName);
-            this.attachmentRepository.save(requestDto.toEntity());
-        }
-    }
-
-    /** 실제 첨부파일 삭제 */
-    private void deleteFiles(List<Attachment> attachmentList) {
-        for (Attachment attachmentEntity : attachmentList) {
-            File file = new File(attachmentEntity.getFilePath());
-            if (file.exists() && file.delete()) {
-                System.out.println("[DELETE] File Removed Success " + attachmentEntity.getFilePath());
-            }
+    /** 실제파일 삭제 */
+    private void deleteFile(Attachment attachment) {
+        File file = new File(attachment.getFilePath());
+        if (file.exists() && file.delete()) {
+            System.out.println("[DELETE] File Removed Successfully. Filepath = " + attachment.getFilePath());
         }
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /** 첨부파일 조회 */
+    /** 첨부파일 목록조회 */
     @Transactional
-    public List<AttachmentResponseDto> findAllByPostId(Long postId) {
-        Post postEntity = this.postRepository.findById(postId).orElseThrow(
-                () -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. post_id = " + postId));
-        List<Attachment> attachmentList = postEntity.getAttachmentList();
-        return attachmentList.stream().map(AttachmentResponseDto::new).collect(Collectors.toList());
+    public List<AttachmentResponseDTO> findAllByPostId(Long postId) {
+        Post post = this.postRepository.findById(postId).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글을 찾을 수 없습니다. post_id = " + postId));
+        List<Attachment> attachments = post.getAttachments();
+        return attachments.stream().map(AttachmentResponseDTO::new).collect(Collectors.toList());
     }
 
     /** 첨부파일 생성 */
     @Transactional
-    public Long save(final Long postId, final List<MultipartFile> files) throws IOException {
-        if (files != null) {
-            Post postEntity = this.postRepository.findById(postId).orElseThrow(
-                    () -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. post_id = " + postId));
-            this.saveFiles(files, postEntity);
+    public Long create(Long postId, List<MultipartFile> files) throws IOException {
+        Post post = this.postRepository.findById(postId).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글을 찾을 수 없습니다. post_id = " + postId));
+        for (MultipartFile file : files) {
+            if (!file.isEmpty()) {
+                AttachmentRequestDTO requestDTO = uploadFile(post, file);
+                this.attachmentRepository.save(requestDTO.toEntity());
+            }
         }
         return postId;
     }
 
     /** 첨부파일 수정 */
     @Transactional
-    public Long update(final Long postId, final List<MultipartFile> files) throws IOException {
-        // 1st Process: Delete All Old Files
-        Post postEntity = this.postRepository.findById(postId).orElseThrow(
-                () -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. post_id = " + postId));
-        List<Attachment> attachmentList = postEntity.getAttachmentList();
-        this.deleteFiles(attachmentList);
-        this.attachmentRepository.deleteAll(attachmentList);
+    public Long update(Long postId, List<Long> attachmentIds, List<MultipartFile> files) throws IOException {
+        Post post = this.postRepository.findById(postId).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글을 찾을 수 없습니다. post_id = " + postId));
+        List<Attachment> attachments = post.getAttachments();
 
-        // 2nd Process: Save All New Files
-        if (files != null) {
-            this.saveFiles(files, postEntity);
+        // 기존 파일 갱신
+        attachments.stream()
+                .filter(attachment -> !attachmentIds.contains(attachment.getId()))
+                .forEach(attachment -> {
+                    this.deleteFile(attachment);
+                    this.attachmentRepository.delete(attachment);
+                });
+
+        // 새 파일 추가
+        for (MultipartFile file : files) {
+            if (!file.isEmpty()) {
+                AttachmentRequestDTO attachmentRequestDTO = uploadFile(post, file);
+                this.attachmentRepository.save(attachmentRequestDTO.toEntity());
+            }
         }
         return postId;
     }
 
     /** 첨부파일 삭제 */
     @Transactional
-    public void deleteAllByPostId(final Long postId) {
-        Post postEntity = this.postRepository.findById(postId).orElseThrow(
-                () -> new IllegalArgumentException("해당 게시글이 존재하지 않습니다. post_id = " + postId));
-        List<Attachment> attachmentList = postEntity.getAttachmentList();
-        this.deleteFiles(attachmentList);
-        this.attachmentRepository.deleteAll(attachmentList);
+    public void deleteAllByPostId(Long postId) {
+        Post post = this.postRepository.findById(postId).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글을 찾을 수 없습니다. post_id = " + postId));
+        List<Attachment> attachments = post.getAttachments();
+        attachments.forEach(this::deleteFile);
+        this.attachmentRepository.deleteAll(attachments);
     }
 }
