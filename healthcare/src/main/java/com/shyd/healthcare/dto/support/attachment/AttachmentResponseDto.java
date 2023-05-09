d563ebd321f77537f797c884ab7689c97212e162
JoSihun <kepler452k@naver.com>
2023-05-09 17:40:05 +0900

    refactor(AttachmentResponseDTO.java): refactor AttachmentResponseDto.java rename class name rename field name refactor field createdDate value refactor field updatedDate value

--- healthcare/src/main/java/com/shyd/healthcare/dto/support/attachment/AttachmentResponseDto.java
+++ healthcare/src/main/java/com/shyd/healthcare/dto/support/attachment/AttachmentResponseDto.java
@@ -3,23 +3,21 @@ package com.shyd.healthcare.dto.support.attachment;
 import com.shyd.healthcare.domain.support.board.Attachment;
 import lombok.Getter;
 
-import java.time.format.DateTimeFormatter;
-
 @Getter
-public class AttachmentResponseDto {
-    private Long id;
-    private Long size;
-    private String fileName;
-    private String filePath;
-    private String createdDate;
-    private String updatedDate;
+public class AttachmentResponseDTO {
+    private final Long id;
+    private final Long fileSize;
+    private final String fileName;
+    private final String filePath;
+    private final String createdDate;
+    private final String updatedDate;
 
-    public AttachmentResponseDto(Attachment entity) {
+    public AttachmentResponseDTO(Attachment entity) {
         this.id = entity.getId();
-        this.size = entity.getSize();
+        this.fileSize = entity.getFileSize();
         this.fileName = entity.getFileName();
         this.filePath = entity.getFilePath();
-        this.createdDate = entity.getCreatedDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
-        this.updatedDate = entity.getUpdatedDate().format(DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss"));
+        this.createdDate = entity.getCreatedDate().toString();
+        this.updatedDate = entity.getUpdatedDate().toString();
     }
 }
