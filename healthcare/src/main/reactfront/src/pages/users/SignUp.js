f45bef229172337fd465586b7ba6e2a2bbfbb458
JoSihun <kepler452k@naver.com>
2023-05-10 19:10:13 +0900

    refactor(SignUp.js): refactor SignUp.js as AuthAPI refactored

--- healthcare/src/main/reactfront/src/pages/users/SignUp.js
+++ healthcare/src/main/reactfront/src/pages/users/SignUp.js
@@ -1,96 +1,144 @@
-import React, { useState } from "react";
-import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
-import { signUp } from "../../api/AuthAPI";
-import backgroundImage from "../..//assets/images/bg_signup.jpg";
+import AuthAPI from "../../api/user/AuthAPI";
+import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect, useRef, useState } from "react";
+import { Button, Card, Col, Container, Form, Image, InputGroup, Row } from "react-bootstrap";
+import bg_signup from "../../assets/images/bg_signup.jpg";
 
 const SignUpForm = (props) => {
-    const [visible, setVisible] = useState(false);
-    const [formData, setFormData] = useState({
+    const navigate = useNavigate();
+    const [failed, setFailed] = useState(false);
+    const [formValues, setFormValues] = useState({
+        name: "",
         email: "",
+        contact: "",
+        birthday: "",
         username: "",
         password: "",
     });
 
     const handleChange = async (e) => {
-        e.preventDefault();
-        setFormData({...formData,
-            [e.target.id]: e.target.value
-        });
+        setFormValues((prevValues) => ({...prevValues,
+            [e.target.name]: e.target.value
+        }));
     }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if (formData.password !== formData.passwordCheck) {
-            setVisible(true);
-            return;
-        }
-
-        signUp(formData)
-        .then(() => {
-            window.location.href = `/signin`;
+        AuthAPI.signup(formValues)
+        .then((response) => {
+            navigate("/signin");
         }).catch((error) => {
+            setFailed(true);
             console.log(error);
         });
     }
 
+    const ERROR_MESSAGE = "회원가입 실패. 아이디와 비밀번호를 확인하세요.";
+
     return (
-        <Form onSubmit={handleSubmit}>
-            <Form.Group className="mb-3" controlId="username">
-                <Form.Label>이름</Form.Label>
-                <Form.Control type="text" placeholder="이름" onChange={handleChange} />
-            </Form.Group>
+        <Card style={{ minWidth: "75%" }}>
+            <Card.Body>
+                <Card.Title className="fs-2 fw-bold fst-italic">
+                    SignUp
+                </Card.Title>
+                <hr/>
 
-            <Form.Group className="mb-3" controlId="email">
-                <Form.Label>이메일</Form.Label>
-                <Form.Control type="email" placeholder="이메일" onChange={handleChange} />
-            </Form.Group>
+                <Form onSubmit={handleSubmit}>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text>이름</InputGroup.Text>
+                        <Form.Control
+                            type="text"
+                            name="name"
+                            value={formValues.name}
+                            onChange={handleChange}
+                            placeholder="홍길동"
+                        />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text>연락처</InputGroup.Text>
+                        <Form.Control
+                            type="text"
+                            name="contact"
+                            value={formValues.contact}
+                            onChange={handleChange}
+                            placeholder="010-1234-5678"
+                        />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text>생년월일</InputGroup.Text>
+                        <Form.Control
+                            type="date"
+                            name="birthday"
+                            value={formValues.birthday}
+                            onChange={handleChange}
+                            placeholder="생년월일"
+                        />
+                    </InputGroup>
+                    <hr/>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text>@</InputGroup.Text>
+                        <Form.Control
+                            type="email"
+                            name="email"
+                            value={formValues.email}
+                            onChange={handleChange}
+                            placeholder="example@example.com"
+                        />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text>아이디</InputGroup.Text>
+                        <Form.Control
+                            type="text"
+                            name="username"
+                            value={formValues.username}
+                            onChange={handleChange}
+                            placeholder="username"
+                        />
+                    </InputGroup>
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text>비밀번호</InputGroup.Text>
+                        <Form.Control
+                            type="password"
+                            name="password"
+                            value={formValues.password}
+                            onChange={handleChange}
+                            placeholder="password"
+                        />
+                    </InputGroup>
 
-            <Form.Group className="mb-3" controlId="password">
-                <Form.Label>비밀번호</Form.Label>
-                <Form.Control type="password" placeholder="비밀번호" onChange={handleChange} />
-                {visible &&
-                    <Form.Text className="text-danger">
-                        비밀번호가 일치하지 않습니다.
-                    </Form.Text>
-                }
-            </Form.Group>
+                    <Form.Group className="mb-3 d-flex justify-content-center align-items-end">
+                        <Button type="submit" variant="dark" style={{ width: "75%" }}>회원가입</Button>
+                    </Form.Group>
 
-            <Form.Group className="mb-3" controlId="passwordCheck">
-                <Form.Label>비밀번호 확인</Form.Label>
-                <Form.Control type="passwordCheck" placeholder="비밀번호 확인" onChange={handleChange} />
-                {visible &&
-                    <Form.Text className="text-danger">
-                        비밀번호가 일치하지 않습니다.
-                    </Form.Text>
-                }
-            </Form.Group>
-            {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group> */}
-            <div className="d-flex justify-content-end">
-                <Button variant="dark" type="submit">SignUp</Button>
-            </div>
-        </Form>
+                    {failed && <div className="text-danger text-center mb-3">{ERROR_MESSAGE}</div>}
+                    <div className="text-center text-secondary">
+                        <p>Do you have an account? <Link to="/signup">Sign in</Link></p>
+                    </div>
+                </Form>
+            </Card.Body>
+        </Card>
     );
 }
 
 export default function SignUp() {
+    const pageRef = useRef(null);
+
+    useEffect(() => {
+        pageRef.current.scrollIntoView({
+            block: "center",
+            inline: "center",
+            behavior: "smooth",
+        });
+    }, []);
+
     return (
-        <Container fluid>
+        <Container fluid ref={pageRef}>
             <Row className="justify-content-center" style={{ minHeight: "100vh" }}>
-                <Col className="col-md-6 d-flex justify-content-center" style={{ background: "black" }}>
-                    <div className="align-self-center text-center" style={{ width: "100%", height: "100%" }}>
-                        <img src={backgroundImage} alt="background" width="75%" />
-                    </div>
+                <Col className="col-md-6 d-flex justify-content-center align-items-center p-0" style={{ background: "black" }}>
+                    <Image src={bg_signup} width="100%" height="100%" />
                 </Col>
-                <Col className="col-md-6 d-flex justify-content-center" style={{ background: "lightgray" }}>
-                    <Card className="align-self-center" style={{ minWidth: "50vh" }}>
-                        <Card.Body>
-                            <Card.Title><h2><strong>SignUp</strong></h2></Card.Title>
-                            <hr/>
-                            <SignUpForm />
-                        </Card.Body>
-                    </Card>
+                <Col className="col-md-6 d-flex justify-content-center align-items-center p-0" style={{ background: "lightgray" }}>
+                    <SignUpForm />
                 </Col>
             </Row>
         </Container>
