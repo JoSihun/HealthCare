5c47a8aaa275f164a5335ff268a1ce244b8bde52
JoSihun <kepler452k@naver.com>
2023-05-15 19:25:23 +0900

    fix(CommentAPI.js): fix params and refactor CommentAPI.js

--- healthcare/src/main/reactfront/src/api/support/CommentAPI.js
+++ healthcare/src/main/reactfront/src/api/support/CommentAPI.js
@@ -4,38 +4,42 @@ const TOKEN_TYPE = localStorage.getItem("tokenType");
 let ACCESS_TOKEN = localStorage.getItem("accessToken");
 let REFRESH_TOKEN = localStorage.getItem("refreshToken");
 
-const CommentAPI = axios.create({
+export const CommentAPI = axios.create({
     // baseURL: 'http://localhost:8080',
     headers: {
         'Content-Type': 'application/json',
         'Authorization': `${TOKEN_TYPE} ${ACCESS_TOKEN}`,
-        'X-Refresh-Token': REFRESH_TOKEN,
+        'X-Refresh-Token': `${REFRESH_TOKEN}`,
     },
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 /** 댓글 목록 */
 export const fetchCommentsV1 = async (postId) => {
-    const response = await CommentAPI.get(`/api/v1/comment?post=${postId}`);
+    const params = { post: postId };
+    const response = await CommentAPI.get(`/api/v1/comment`, { params });
     return response.data;
 }
 
 /** 댓글 삽입 */
 export const createCommentV1 = async (postId, data) => {
-    const response = await CommentAPI.post(`/api/v1/comment?post=${postId}`, data);
+    const params = { post: postId };
+    const response = await CommentAPI.post(`/api/v1/comment`, data, { params });
     return response.data;
 }
 
 /** 댓글 수정 */
-export const updateCommentV1 = async (postId, commentId, data) => {
-    const queryString = `post=${postId}&comment=${commentId}`;
-    const response = await CommentAPI.put(`/api/v1/comment?${queryString}`, data);
+export const updateCommentV1 = async (id, data) => {
+    const response = await CommentAPI.put(`/api/v1/comment/${id}`, data);
     return response.data;
 }
 
 /** 댓글 삭제 */
-export const deleteCommentV1 = async (postId, commentId) => {
-    const queryString = `post=${postId}&comment=${commentId}`;
-    const response = await CommentAPI.delete(`/api/v1/comment?${queryString}`);
+export const deleteCommentV1 = async (id) => {
+    const response = await CommentAPI.delete(`/api/v1/comment/${id}`);
     return response.data;
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+const ApiObject = { CommentAPI, fetchCommentsV1, createCommentV1, updateCommentV1, deleteCommentV1 };
+export default ApiObject;
