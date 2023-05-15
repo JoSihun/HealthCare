149ad29f3455c49f679b2c9049815b8d2e13b5a8
JoSihun <kepler452k@naver.com>
2023-05-15 16:42:50 +0900

    refactor(QNABoard.js): refactor QNABoard.js

--- healthcare/src/main/reactfront/src/pages/support/QNABoard.js
+++ healthcare/src/main/reactfront/src/pages/support/QNABoard.js
@@ -1,50 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { Link, useSearchParams } from "react-router-dom";
 import { Button, Card, Col, Container, Row, Table } from "react-bootstrap";
-import Paging from "../../components/support/Paging";
-import { fetchPageV1 } from "../../api/PostAPI";
+
+import PostAPI from "../../api/support/PostAPI";
+import PageNavigation from "../../components/PageNavigation";
 import { SupportSideBar } from "../../components/SideBar";
 
-const SelectSize = (props) => {
-    const handleSelect = async (e) => {
-        e.preventDefault();
-        props.searchParams.set("page", parseInt(props.pages.pageable.offset / e.target.value) + 1);
-        props.searchParams.set("size", e.target.value);
-        props.setSearchParams(props.searchParams);
-    }
-
-    return (
-        <div className="d-flex justify-content-end mb-3">
-            <div className="me-2">보기 옵션</div>
-            <select onChange={handleSelect} value={props.size}>
-                <option value={10}>10개씩</option>
-                <option value={20}>20개씩</option>
-                <option value={30}>30개씩</option>
-                <option value={50}>50개씩</option>
-                <option value={100}>100개씩</option>
-            </select>
-        </div>
-    );
-}
-
 const Search = (props) => {
+    const [searchParams] = useSearchParams();
     const [searchValue, setSearchValue] = useState("");
     const [searchFilter, setSearchFilter] = useState("Title");
 
     const handleValue = async (e) => {
-        e.preventDefault();
         setSearchValue(e.target.value);
     }
 
     const handleFilter = async (e) => {
-        e.preventDefault();
         setSearchFilter(e.target.value);
     }
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const queryString = `searchFilter=${searchFilter}&searchValue=${searchValue}`;
-        window.location.href = `/support/qnaboard/search?${queryString}`;
+        searchParams.set("searchValue", searchValue);
+        searchParams.set("searchFilter", searchFilter);
+        window.location.assign(`/support/qnaboard/search?${searchParams.toString()}`);
     }
 
     return (
@@ -71,86 +50,129 @@ const Search = (props) => {
     );
 }
 
+const Select = (props) => {
+    const { data, setPage, setSize } = props;
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const handleSelect = async (e) => {
+        searchParams.set("page", Math.floor(data.pageable.offset / e.target.value) + 1);
+        searchParams.set("size", e.target.value);
+        setSearchParams(searchParams);
+        setPage(searchParams.get("page"));
+        setSize(searchParams.get("size"));
+    }
+
+    return (
+        <div className="d-flex justify-content-end mb-3">
+            <div className="me-2">보기 옵션</div>
+            <select onChange={handleSelect} defaultValue={20}>
+                <option value={10}>10개씩</option>
+                <option value={20}>20개씩</option>
+                <option value={30}>30개씩</option>
+                <option value={50}>50개씩</option>
+                <option value={100}>100개씩</option>
+            </select>
+        </div>
+    );
+}
+
 const QNABoardList = (props) => {
-    const { posts } = props;
+    const { data } = props;
+
+    const convertDate = (prevDate) => {
+        const now = new Date();
+        const date = new Date(prevDate);
+        const diff = Math.round((now - date) / (1000 * 60));
+        
+        const year = date.getFullYear();
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const day = date.getDate().toString().padStart(2, '0');
+        
+        if (diff <= 10) {
+            const hour = date.getHours().toString().padStart(2, '0');
+            const minute = date.getMinutes().toString().padStart(2, '0');
+            return `${year}-${month}-${day} ${hour}:${minute}`;
+        }
+        return `${year}-${month}-${day}`;
+      }
 
     return (
         <Table responsive hover border="2px">
             <thead>
                 <tr style={{ color: "white", backgroundColor: "black" }}>
-                    <th className="text-left">#</th>
-                    <th className="text-left">제목</th>
-                    <th className="text-center">조회수</th>
-                    <th className="text-center">작성자</th>
-                    <th className="text-center">작성일</th>
+                    <th className="text-center" style={{ width: "5%"}} >#</th>
+                    <th className="text-center" style={{ width: "55%"}} >제목</th>
+                    <th className="text-center" style={{ width: "10%"}} >조회수</th>
+                    <th className="text-center" style={{ width: "10%"}} >작성자</th>
+                    <th className="text-center" style={{ width: "20%"}} >작성일</th>
                 </tr>
             </thead>
             <tbody>
-                {posts.map((post, index) => (
+                {data.content && data.content.map((post, index) => (
                     <tr key={index}>
-                        <td className="text-left">{post.id}</td>
-                        <td className="text-left">
+                        <td className="text-center">{post.id}</td>
+                        <td className="text-start">
                             <Link to={`/support/qnaboard/post/${post.id}`} style={{ color: "black", textDecoration: "none" }}>
                                 {post.title}
                             </Link>
                         </td>
                         <td className="text-center">{post.hits}</td>
                         <td className="text-center">{post.author}</td>
-                        <td className="text-center">{post.createdDate}</td>
+                        <td className="text-center">{convertDate(post.createdDate)}</td>
                     </tr>
                 ))}
             </tbody>
         </Table>
-    );
+    )
 }
 
-export default function QNABoard() {
-    const [posts, setPosts] = useState([]);
-    const [pages, setPages] = useState({});
-    const [searchParams, setSearchParams] = useSearchParams();
-    const page = searchParams.get("page") ? searchParams.get("page") : 1;
-    const size = searchParams.get("size") ? searchParams.get("size") : 20;
+const QNABoardBody = (props) => {
+    const [data, setData] = useState({});
+    const [searchParams] = useSearchParams();
+    const [page, setPage] = useState(searchParams.get("page") || 1);
+    const [size, setSize] = useState(searchParams.get("size") || 20);
 
     useEffect(() => {
-        const queryString = `page=${page - 1}&size=${size}`;
-        fetchPageV1("qna-board", queryString)
-        .then((response) => {
-            setPages(response);
-            setPosts(response.content);
-        }).catch((error) => {
-            console.log(error);
-        });
+        PostAPI.fetchPostPage("qna-board", { page, size })
+        .then(response => setData(response))
+        .catch(error => console.log(error));
     }, [page, size]);
 
     return (
-        <div>
+        <Card>
+            <Card.Body>
+                <Card.Title className="fs-2 fw-bold">
+                    Q&A
+                </Card.Title>
+                <hr/>
+                <Select data={data} setSize={setSize} setPage={setPage} />
+                <QNABoardList data={data} />
+
+                <div className="d-flex justify-content-end">
+                    <Link to={`/support/qnaboard/form`}>
+                        <Button variant="dark" style={{ width: "100px"}}>글쓰기</Button>
+                    </Link>
+                </div>
+
+                <PageNavigation data={data} setPage={setPage} />
+                <Search />
+            </Card.Body>
+        </Card>
+    )
+}
+
+export default function QNABoard() {
+    return (
         <Container fluid>
             <Row className="justify-content-center">
-                <Col className="col-md-2 mx-2 my-4">
+                <Col className="col-md-2 mx-1 my-4">
                     <SupportSideBar />    
                 </Col>
 
-                <Col className="col-md-9 mx-2 my-4">
-                    <Card>
-                        <Card.Body>
-                            <Card.Title><h2><strong>Q&A</strong></h2></Card.Title>
-                            <hr/>
-                            <SelectSize pages={pages} size={size} searchParams={searchParams} setSearchParams={setSearchParams} />
-
-                            <QNABoardList posts={posts} />
-                            <div className="d-flex justify-content-end">
-                                <Link to={"/support/qnaboard/form"}>
-                                    <Button variant="dark" style={{ width: "100px" }}>질문등록</Button>
-                                </Link>
-                            </div>
-
-                            <Paging pages={pages} searchParams={searchParams} setSearchParams={setSearchParams} />
-                            <Search searchParams={searchParams} setSearchParams={setSearchParams} />
-                        </Card.Body>
-                    </Card>
+                <Col className="col-md-9 mx-1 my-4">
+                    <QNABoardBody />
                 </Col>
             </Row>
         </Container>
-        </div>
     );
 }
