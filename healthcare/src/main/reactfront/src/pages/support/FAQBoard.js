d28c25e505e3236125f6bfec6136d6487f9d29c6
JoSihun <kepler452k@naver.com>
2023-05-18 19:07:18 +0900

    refactor(FAQBoard.js): refactor FAQBoard.js

--- healthcare/src/main/reactfront/src/pages/support/FAQBoard.js
+++ healthcare/src/main/reactfront/src/pages/support/FAQBoard.js
@@ -29,34 +29,36 @@ const AddForm = (props) => {
     }
     
     return (showAddForm ? (
-        <Row className="justify-content-center">
-            <Card>
-                <Card.Body>
-                    <Card.Title className="fs-2 fw-bold fst-italic">
-                        FAQ 추가
-                    </Card.Title>
-                    <hr/>
-                    <form onSubmit={handleSubmit}>
-                        <div className="form-group mb-3">
-                            <label htmlFor="title"><h5><strong>자주 묻는 질문</strong></h5></label>
-                            <input className="form-control" type="text" id="title" name="title" onChange={handleChange} value={formValues.title} />
+        <Card className="border-secondary">
+            <Card.Body>
+                <Card.Title className="fs-2 fw-bold fst-italic">
+                    FAQ 추가
+                </Card.Title>
+                <hr/>
+                <form onSubmit={handleSubmit}>
+                    <div className="form-group mb-3">
+                        <label htmlFor="title"><h5><strong>자주 묻는 질문</strong></h5></label>
+                        <input className="form-control" type="text" id="title" name="title" onChange={handleChange} value={formValues.title} />
+                    </div>
+                    <div className="form-group mb-3">
+                        <label htmlFor="content"><h5><strong>자주 묻는 질문 답변</strong></h5></label>
+                        <textarea className="form-control" type="text" id="content" name="content" rows={3} onChange={handleChange} value={formValues.content} />
+                    </div>
+                    <div className="form-group d-flex justify-content-end">
+                        <div className="col-2 d-flex justify-content-center">
+                            <Button className="me-1" onClick={handleClick} variant="danger" style={{ width: "100%" }}>취소</Button>
+                            <Button className="ms-1" type="submit" variant="dark" style={{ width: "100%" }}>추가</Button>
                         </div>
-                        <div className="form-group mb-3">
-                            <label htmlFor="content"><h5><strong>자주 묻는 질문 답변</strong></h5></label>
-                            <textarea className="form-control" type="text" id="content" name="content" rows={3} onChange={handleChange} value={formValues.content} />
-                        </div>
-                        <div className="form-group d-flex justify-content-end">
-                            <Button className="me-1" onClick={handleClick} variant="danger" style={{ width: "8%" }}>취소</Button>
-                            <Button className="ms-1" type="submit" variant="dark" style={{ width: "8%" }}>추가</Button>
-                        </div>
-                    </form>
-                </Card.Body>
-            </Card>
-        </Row>
+                    </div>
+                </form>
+            </Card.Body>
+        </Card>
     ) : (
-        <Row className="justify-content-end">
-            <Button onClick={handleClick} variant="dark" style={{ width: "8%" }}>추가</Button>
-        </Row>
+        <div className="d-flex justify-content-end">
+            <div className="col-1 d-flex justify-content-center">
+                <Button onClick={handleClick} variant="dark" style={{ width: "100%" }}>추가</Button>
+            </div>
+        </div>
     ));
 }
 
@@ -90,29 +92,31 @@ const EditForm = (props) => {
     }
     
     return (showEditForm ? (
-        <Row className="justify-content-center mt-3">
+        <div className="border border-2 rounded mt-3 p-3">
             <form onSubmit={handleSubmit}>
-                <div className="border rounded mb-3 p-3">
-                    <div className="form-group input-group mb-3">
-                        <label className="input-group-text"><h5><strong>Q.</strong></h5></label>
-                        <input className="form-control" type="text" id="title" name="title" onChange={handleChange} value={formValues.title} />
-                    </div>
-                    <div className="form-group input-group mb-3">
-                        <label className="input-group-text"><h5><strong>A.</strong></h5></label>
-                        <textarea className="form-control" type="text" id="content" name="content" rows={3} onChange={handleChange} value={formValues.content} />
-                    </div>
+                <div className="form-group input-group mb-3">
+                    <label className="input-group-text"><h5><strong>Q.</strong></h5></label>
+                    <input className="form-control" type="text" id="title" name="title" onChange={handleChange} value={formValues.title} />
+                </div>
+                <div className="form-group input-group mb-3">
+                    <label className="input-group-text"><h5><strong>A.</strong></h5></label>
+                    <textarea className="form-control" type="text" id="content" name="content" rows={3} onChange={handleChange} value={formValues.content} />
                 </div>
                 <div className="form-group d-flex justify-content-end">
-                    <Button className="me-1" onClick={handleClick} variant="danger" style={{ width: "8%" }}>취소</Button>
-                    <Button className="ms-1" type="submit" variant="primary" style={{ width: "8%" }}>수정</Button>
+                    <div className="col-2 d-flex justify-content-center">
+                        <Button className="me-1" onClick={handleClick} variant="danger" style={{ width: "100%" }}>취소</Button>
+                        <Button className="ms-1" type="submit" variant="primary" style={{ width: "100%" }}>수정</Button>
+                    </div>
                 </div>
             </form>
-        </Row>
+        </div>
     ) : (
-        <Row className="justify-content-end">
-            <Button className="me-1" onClick={handleClick} variant="primary" style={{ width: "8%" }}>수정</Button>
-            <Button className="ms-1" onClick={handleDelete} variant="danger" style={{ width: "8%" }}>삭제</Button>
-        </Row>
+        <div className="d-flex justify-content-end">
+            <div className="col-2 d-flex justify-content-center">
+                <Button className="me-1" onClick={handleClick} variant="primary" style={{ width: "100%" }}>수정</Button>
+                <Button className="ms-1" onClick={handleDelete} variant="danger" style={{ width: "100%" }}>삭제</Button>                
+            </div>
+        </div>
     ));
 }
 
@@ -126,7 +130,7 @@ const FAQBoardItem = (props) => {
         .catch(error => console.log(error));
     }, [id]);
 
-    return (post &&
+    return (
         <Accordion.Item eventKey={post.id}>
             <Accordion.Header>
                 <h3><strong>Q. {post.title}</strong></h3>
@@ -140,44 +144,43 @@ const FAQBoardItem = (props) => {
 }
 
 const FAQBoardList = (props) => {
-    const [pageData, setPageData] = useState({});
+    const [data, setData] = useState({});
 
     useEffect(() => {
-        PostAPI.fetchPostPage("faq-board")
+        PostAPI.fetchPostPage("faq-board", { size: 10 })
         .then((response) => {
-            setPageData(response);
+            setData(response);
         }).catch((error) => {
             console.log(error);
         });
     }, []);
 
     return (
-        <Row className="justify-content-center mb-4">
-            <Card style={{ minHeight: "50vh" }}>
-                <Card.Body>
-                    <Card.Title className="fs-2 fw-bold fst-italic">
-                        FAQ
-                    </Card.Title>
-                    <hr/>
-                    <Accordion>
-                        {pageData.content && pageData.content.map((item, index) => (
-                            <FAQBoardItem id={item.id} key={index} />
-                        ))}
-                    </Accordion>
-                </Card.Body>
-            </Card>
-        </Row>
-    )
+        <Card className="vh-50 mb-3">
+            <Card.Body>
+                <Card.Title className="fs-2 fw-bold fst-italic">
+                    FAQ
+                </Card.Title>
+                <hr/>
+                <Accordion>
+                    {data.content && data.content.map((item, index) => (
+                        <FAQBoardItem id={item.id} key={index} />
+                    ))}
+                </Accordion>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default function FAQBoard() {
     return (
         <Container fluid>
-            <Row className="justify-content-center">
-                <Col className="col-md-2 mx-1 my-4">
+            <Row className="justify-content-center mt-3">
+                <Col className="col-12 col-lg-2 mb-3">
                     <SupportSideBar />
                 </Col>
-                <Col className="col-md-9 mx-1 my-4">
+
+                <Col className="col-12 col-lg-9 mb-3">
                     <FAQBoardList />
                     <AddForm />
                 </Col>
