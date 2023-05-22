d422cd558a64fb650f8b8c287aac38ded027359e
JoSihun <kepler452k@naver.com>
2023-05-22 19:54:39 +0900

    feat(controller): Update Controllers as services have changed
    - ChatRoomRestController.java:
      - Fix function readChatRoomList() to fetchChatRoom() using headers
      - Fix function readChatRoomList() to fetchChatRoom() due to changes in ChatRoomService.java
      - Add function fetchChatRoom() with return type Page
      - Refactor function readAllChatRoomList() to fetchChatRoom() due to changes in ChatRoomService.java
      - Add function fetchChatRoom() with return type Page for admin-only GET mapping
      - Refactor two different types of functions readChatRoom()
      - Refactor function saveChatRoom()
      - Refactor function updateChatRoom()
    
    - ChatMessageRestController.java:
      - Fix function receiveMessage() to sendChatMessage() with modified internal implementation
      - Refactor to remove unnecessary function receiveNewMessage() as this functionality is implemented in sendChatMessage()
      - Refactor combined two different types of functions readChatMessageList() into fetchChatMessages()

--- healthcare/src/main/java/com/shyd/healthcare/controller/support/ChatMessageRestController.java
+++ healthcare/src/main/java/com/shyd/healthcare/controller/support/ChatMessageRestController.java
@@ -1,75 +1,108 @@
 package com.shyd.healthcare.controller.support;
 
-import com.shyd.healthcare.dto.support.livechat.ChatMessageRequestDto;
-import com.shyd.healthcare.dto.support.livechat.ChatMessageResponseDto;
-import com.shyd.healthcare.dto.support.livechat.ChatRoomResponseDto;
+import com.shyd.healthcare.dto.support.livechat.ChatMessageRequestDTO;
+import com.shyd.healthcare.dto.support.livechat.ChatMessageResponseDTO;
+import com.shyd.healthcare.dto.support.livechat.ChatRoomRequestDTO;
 import com.shyd.healthcare.service.support.ChatMessageService;
 import com.shyd.healthcare.service.support.ChatRoomService;
+import com.shyd.healthcare.service.user.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @RestController
+@RequiredArgsConstructor
 public class ChatMessageRestController {
+    private final UserService userService;
     private final ChatRoomService chatRoomService;
     private final ChatMessageService chatMessageService;
+    private final SimpMessagingTemplate messagingTemplate;
     private final SimpMessageSendingOperations sendingOperations;
 
-    /** WebSocket Controller 기본 양식 */
+//    /** WebSocket Controller 기본 양식 */
 //    @MessageMapping("/chat")
 //    public void receiveMessage(@Payload ChatMessageRequestDto requestDto) {
 //        sendingOperations.convertAndSend("/sub/chat", requestDto);
 //    }
+//
+//    @MessageMapping("/chat")                // subscribe, publish url
+//    public void receiveMessage(@Payload ChatMessageRequestDTO requestDTO) {
+//        if (requestDTO.getRoomUuid() == null) {
+//            Long chatRoomId = this.chatRoomService.create(requestDto.getSender());
+//            ChatRoomResponseDTO chatRoomResponseDto = this.chatRoomService.findById(chatRoomId);
+//
+//            requestDto.setRoomUuid(chatRoomResponseDto.getUuid());
+//            Long chatMessageId = this.chatMessageService.save(chatRoomId, requestDto);
+//            ChatMessageResponseDTO chatMessageResponseDto = this.chatMessageService.findById(chatMessageId);
+//
+//            String subscribeChannel = "/sub/chat/" + chatMessageResponseDto.getSender();
+//            sendingOperations.convertAndSend(subscribeChannel, chatMessageResponseDto);
+//        } else {
+//            Long chatRoomId = this.chatRoomService.findByUuid(requestDto.getRoomUuid()).getId();
+//            Long chatMessageId = this.chatMessageService.save(chatRoomId, requestDto);
+//            ChatMessageResponseDTO chatMessageResponseDto = this.chatMessageService.findById(chatMessageId);
+//
+//            String subscribeChannel = "/sub/chat/" + chatMessageResponseDto.getRoomUuid();
+//            sendingOperations.convertAndSend(subscribeChannel, chatMessageResponseDto);
+//        }
+//    }
+//
+//    @MessageMapping("/chat/create")
+//    public void receiveNewMessage(@Payload ChatMessageRequestDTO requestDto) {
+//        Long chatRoomId = this.chatRoomService.findByUuid(requestDto.getRoomUuid()).getId();
+//        Long chatMessageId = this.chatMessageService.save(chatRoomId, requestDto);
+//        ChatRoomResponseDTO chatRoomResponseDto = this.chatRoomService.findById(chatRoomId);
+//        ChatMessageResponseDTO chatMessageResponseDto = this.chatMessageService.findById(chatMessageId);
+//
+//        String subscribeChannel = "/sub/chat/" + chatRoomResponseDto.getUuid();
+//        sendingOperations.convertAndSend(subscribeChannel, chatMessageResponseDto);
+//    }
 
-    @MessageMapping("/chat")                // subscribe, publish url
-    public void receiveMessage(@Payload ChatMessageRequestDto requestDto) {
-        if (requestDto.getRoomUuid() == null) {
-            Long chatRoomId = this.chatRoomService.create(requestDto.getSender());
-            ChatRoomResponseDto chatRoomResponseDto = this.chatRoomService.findById(chatRoomId);
-
-            requestDto.setRoomUuid(chatRoomResponseDto.getUuid());
-            Long chatMessageId = this.chatMessageService.save(chatRoomId, requestDto);
-            ChatMessageResponseDto chatMessageResponseDto = this.chatMessageService.findById(chatMessageId);
-
-            String subscribeChannel = "/sub/chat/" + chatMessageResponseDto.getSender();
-            sendingOperations.convertAndSend(subscribeChannel, chatMessageResponseDto);
+    /** ChatMessage 전송 - STOMP 웹소켓 요청 */
+    @MessageMapping("/chat/{channel}")
+    public void sendChatMessage(@Header("Authorization") String accessToken,
+                                @DestinationVariable("channel") String channel,
+                                @Payload ChatMessageRequestDTO chatMessageRequestDTO) {
+        if (channel.equals("null")) {
+            // CREATE A NEW CHAT ROOM IF CHANNEL IS NULL
+            channel = this.userService.findByAccessToken(accessToken).getId().toString();
+            ChatRoomRequestDTO chatRoomRequestDTO = new ChatRoomRequestDTO();
+            Long chatRoomId = this.chatRoomService.create(accessToken, chatRoomRequestDTO);
+            Long chatMessageId = this.chatMessageService.create(accessToken, chatRoomId, chatMessageRequestDTO);
+            ChatMessageResponseDTO chatMessageResponseDTO = this.chatMessageService.findById(chatMessageId);
+            messagingTemplate.convertAndSend("/sub/chat/" + channel, chatMessageResponseDTO);
         } else {
-            Long chatRoomId = this.chatRoomService.findByUuid(requestDto.getRoomUuid()).getId();
-            Long chatMessageId = this.chatMessageService.save(chatRoomId, requestDto);
-            ChatMessageResponseDto chatMessageResponseDto = this.chatMessageService.findById(chatMessageId);
-
-            String subscribeChannel = "/sub/chat/" + chatMessageResponseDto.getRoomUuid();
-            sendingOperations.convertAndSend(subscribeChannel, chatMessageResponseDto);
+            // CREATE A NEW CHAT MESSAGE IF CHANNEL IS NOT NULL
+            Long chatRoomId = this.chatRoomService.findByUuid(channel).getId();
+            Long chatMessageId = this.chatMessageService.create(accessToken, chatRoomId, chatMessageRequestDTO);
+            ChatMessageResponseDTO chatMessageResponseDTO = this.chatMessageService.findById(chatMessageId);
+            messagingTemplate.convertAndSend("/sub/chat/" + channel, chatMessageResponseDTO);
         }
     }
 
-    @MessageMapping("/chat/create")
-    public void receiveNewMessage(@Payload ChatMessageRequestDto requestDto) {
-        Long chatRoomId = this.chatRoomService.findByUuid(requestDto.getRoomUuid()).getId();
-        Long chatMessageId = this.chatMessageService.save(chatRoomId, requestDto);
-        ChatRoomResponseDto chatRoomResponseDto = this.chatRoomService.findById(chatRoomId);
-        ChatMessageResponseDto chatMessageResponseDto = this.chatMessageService.findById(chatMessageId);
-
-        String subscribeChannel = "/sub/chat/" + chatRoomResponseDto.getUuid();
-        sendingOperations.convertAndSend(subscribeChannel, chatMessageResponseDto);
+    /** ChatMessage 목록조회 - STOMP 웹소켓 요청 */
+    @SubscribeMapping("/sub/chat/{channel}")
+    public List<ChatMessageResponseDTO> loadChatMessages(@DestinationVariable("channel") String channel) {
+        return this.chatMessageService.findAllByChatRoomUuid(channel, "asc");
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /** 특정 채팅방 LiveChat Message 목록조회 - Id 검색 */
-    @GetMapping("/api/v1/livechat/message/{id}")
-    public List<ChatMessageResponseDto> readChatMessageList(@PathVariable Long id) {
-        return this.chatMessageService.findAllByChatRoomIdAsc(id);
-    }
-
-    /** 특정 채팅방 LiveChat Message 목록조회 - Uuid 검색 */
+    /** ChatMessage 목록조회 - 특정 ChatRoom id, uuid 검색, List */
     @GetMapping("/api/v1/livechat/message")
-    public List<ChatMessageResponseDto> readChatMessageList(@RequestParam(value = "uuid") String chatRoomUuid) {
-        // 아마도 최초 연결 지점, 여기까지 수정했음
-        return this.chatMessageService.findAllByChatRoomUuidAsc(chatRoomUuid);
+    public List<ChatMessageResponseDTO> fetchChatMessages(@RequestParam(value = "chatRoom", required = false) Long chatRoomId,
+                                                          @RequestParam(value = "uuid", required = false) String chatRoomUuid,
+                                                          @RequestParam(value = "sort", required = false, defaultValue = "asc") String sort) {
+        if (chatRoomId != null) {
+            return this.chatMessageService.findAllByChatRoomId(chatRoomId, sort);
+        }
+        return this.chatMessageService.findAllByChatRoomUuid(chatRoomUuid, sort);
     }
 }
--- healthcare/src/main/java/com/shyd/healthcare/controller/support/ChatRoomRestController.java
+++ healthcare/src/main/java/com/shyd/healthcare/controller/support/ChatRoomRestController.java
@@ -1,59 +1,79 @@
 package com.shyd.healthcare.controller.support;
 
-import com.shyd.healthcare.dto.support.livechat.ChatRoomRequestDto;
-import com.shyd.healthcare.dto.support.livechat.ChatRoomResponseDto;
+import com.shyd.healthcare.dto.support.livechat.ChatRoomRequestDTO;
+import com.shyd.healthcare.dto.support.livechat.ChatRoomResponseDTO;
 import com.shyd.healthcare.service.support.ChatRoomService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequiredArgsConstructor
 @RestController
+@RequiredArgsConstructor
 public class ChatRoomRestController {
     private final ChatRoomService chatRoomService;
 
-    /** LiveChat Room 목록조회 - 고객용 */
-    @GetMapping("/api/v1/livechat/list/{userId}")
-    public List<ChatRoomResponseDto> readChatRoomList(@PathVariable String userId) {
-        return this.chatRoomService.findAllByUserIdDesc(userId);
+    /** ChatRoom 목록조회 - 고객용, List */
+    @GetMapping("/api/v1/livechat/list")
+    public List<ChatRoomResponseDTO> fetchChatRoom(@RequestHeader("Authorization") String accessToken,
+                                                   @RequestParam(value = "sort", required = false, defaultValue = "desc") String sort) {
+        return this.chatRoomService.findAllByUserId(accessToken, sort);
     }
 
-    /** LiveChat Room 목록조회 - 관리자용 */
-    @GetMapping("/api/v1/admin/livechat/list")
-    public List<ChatRoomResponseDto> readAllChatRoomList() {
-        return this.chatRoomService.findALlDesc();
+    /** ChatRoom 목록조회 - 고객용, Page */
+    @GetMapping("/api/v1/livechat/page")
+    public Page<ChatRoomResponseDTO> fetchChatRoom(@RequestHeader("Authorization") String accessToken,
+                                                   @RequestParam(value = "page", required = false, defaultValue = "1") Integer page,
+                                                   @RequestParam(value = "size", required = false, defaultValue = "10") Integer size,
+                                                   @RequestParam(value = "sort", required = false, defaultValue = "desc") String sort) {
+        return this.chatRoomService.findAllByUserId(accessToken, page, size, sort);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /** LiveChat Room 조회 - Uuid 검색 */
+    /** ChatRoom 목록조회 - 관리자용, List */
+    @GetMapping("/api/v1/admin/livechat/list")
+    public List<ChatRoomResponseDTO> fetchChatRoom(@RequestParam(value = "sort", required = false, defaultValue = "desc") String sort) {
+        return this.chatRoomService.findAll(sort);
+    }
+
+    /** ChatRoom 목록조회 - 관리자용, Page */
+    @GetMapping("/api/v1/admin/livechat/page")
+    public Page<ChatRoomResponseDTO> fetchChatRoom(@RequestParam(value = "page", required = false, defaultValue = "1") Integer page,
+                                                   @RequestParam(value = "size", required = false, defaultValue = "10") Integer size,
+                                                   @RequestParam(value = "sort", required = false, defaultValue = "desc") String sort) {
+        return this.chatRoomService.findAll(page, size, sort);
+    }
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /** ChatRoom 조회 - Uuid 검색 */
     @GetMapping("/api/v1/livechat/room")
-    public ChatRoomResponseDto readChatRoom(@RequestParam(value = "uuid") String uuid) {
+    public ChatRoomResponseDTO readChatRoom(@RequestParam(value = "uuid") String uuid) {
         return this.chatRoomService.findByUuid(uuid);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-    /** LiveChat Room 조회 - id 검색 */
+    /** ChatRoom 조회 */
     @GetMapping("/api/v1/livechat/room/{id}")
-    public ChatRoomResponseDto readChatRoom(@PathVariable Long id) {
+    public ChatRoomResponseDTO readChatRoom(@PathVariable Long id) {
         return this.chatRoomService.findById(id);
     }
 
-    /** LiveChat Room 생성 */
+    /** ChatRoom 생성 */
     @PostMapping("/api/v1/livechat/room")
-    public Long saveChatRoom(@RequestBody ChatRoomRequestDto requestDto) {
-        return this.chatRoomService.save(requestDto);
+    public Long saveChatRoom(@RequestHeader("Authorization") String accessToken,
+                             @RequestBody ChatRoomRequestDTO requestDTO) {
+        return this.chatRoomService.create(accessToken, requestDTO);
     }
 
-    /** LiveChat Room 수정 */
+    /** ChatRoom 수정 */
     @PutMapping("/api/v1/livechat/room/{id}")
-    public Long updateChatRoom(@PathVariable(value = "id") Long chatRoomId,
-                               @RequestBody ChatRoomRequestDto requestDto) {
-        return this.chatRoomService.update(chatRoomId, requestDto);
+    public Long updateChatRoom(@PathVariable Long id,
+                               @RequestBody ChatRoomRequestDTO requestDTO) {
+        return this.chatRoomService.update(id, requestDTO);
     }
 
-    /** LiveChat Room 삭제 */
+    /** ChatRoom 삭제 */
     @DeleteMapping("/api/v1/livechat/room/{id}")
     public void deleteChatRoom(@PathVariable Long id) {
         this.chatRoomService.delete(id);
