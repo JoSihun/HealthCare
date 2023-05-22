0658398daf802f53dce0459e02f9214de8bc1321
JoSihun <kepler452k@naver.com>
2023-05-22 21:21:43 +0900

    feat(NotFound.js): Update 404 Not Found page

--- healthcare/src/main/reactfront/src/components/NotFound.js
+++ healthcare/src/main/reactfront/src/components/NotFound.js
@@ -1,22 +1,31 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 export default function NotFound() {
     return (
-        <container fluid>
-            <div className="d-flex justify-content-center" style={{ minHeight: "50vh" }}>
-                <div className="align-self-center">
-                    <div style={{ color: "red" }}>
-                        <h1><strong>
-                            [404 ERROR] Page NotFound
-                        </strong></h1>
+        <Container fluid>
+            <Row className="my-3">
+                <Col className="col-12 col-lg-6">
+                    <img className="img-fluid"
+                    alt="404 Not Found"
+                    // src="https://blog.thomasnet.com/hubfs/shutterstock_774749455.jpg"
+                    src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7888.jpg?w=2000"
+                    />
+                </Col>
+                <Col className="col-12 col-lg-6 d-flex justify-content-center align-items-center">
+                    <div className="border-top border-bottom border-danger border-2 rounded p-4">
+                        <div className="d-flex justify-content-center mb-1">
+                            <h1 className="display-4 fw-bold text-secondary">Oops! Page Not Found</h1>
+                        </div>
+                        <div className="d-flex justify-content-center mb-2">
+                            <h4 className="fw-bold text-primary">The page you are looking for does not exist.</h4>
+                        </div>
+                        <div className="d-flex justify-content-center">
+                            <Button className="fw-bold" variant="dark" href="/">Go Back to Homepage</Button>
+                        </div>
                     </div>
-                    <div style={{ color: "black" }}>
-                        <h1><strong><i>
-                            페이지를 찾을 수 없습니다.<br/>
-                        </i></strong></h1>
-                    </div>
-                </div>
-            </div>
-        </container>
+                </Col>
+            </Row>
+        </Container>
     );
 }
