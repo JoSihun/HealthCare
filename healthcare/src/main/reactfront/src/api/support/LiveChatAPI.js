ba97f1c61a4f0caf54ee9ad88b336e01d9ee07eb
JoSihun <kepler452k@naver.com>
2023-05-22 20:02:05 +0900

    feat(api): Update LiveChatAPI and related functions
    - Refactor LiveChatAPI headers to use string interpolation for tokens
    - Update fetchChatRooms() to not require userId parameter
    - Update fetchAdminChatRooms() to not require any parameters
    - Refactor fetchChatRoom() to fetchChatRoomById() and fetchChatRoomByUuid() with respective parameters
    - Update updateChatRoom() to use id parameter instead of chatRoomId
    - Add createChatRoom() function
    - Update fetchChatMessages() to support both roomId and uuid as optional parameters
    - Remove unused deleteChatRoom() overload

--- healthcare/src/main/reactfront/src/api/support/LiveChatAPI.js
+++ healthcare/src/main/reactfront/src/api/support/LiveChatAPI.js
@@ -9,44 +9,71 @@ export const LiveChatAPI = axios.create({
     headers: {
         'Content-Type': 'application/json',
         'Authorization': `${TOKEN_TYPE} ${ACCESS_TOKEN}`,
-        'X-Refresh-Token': REFRESH_TOKEN,
+        'X-Refresh-Token': `${REFRESH_TOKEN}`,
     },
 });
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
-/** USER 채팅방 목록 */
-export const fetchChatRooms = async (userId) => {
-    const response = await LiveChatAPI.get(`/api/v1/livechat/list/${userId}`);
+/** USER 채팅방 목록 - List */
+export const fetchChatRooms = async () => {
+    const response = await LiveChatAPI.get(`/api/v1/livechat/list`);
     return response.data;
 }
 
-/** 채팅방 메세지 목록 */
-export const fetchChatMessages = async (roomUuid) => {
-    const response = await LiveChatAPI.get(`/api/v1/livechat/message?uuid=${roomUuid}`);
-    return response.data;
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////
-/** USER 채팅방 조회 */
-export const fetchChatRoom = async (roomUuid) => {
-    const response = await LiveChatAPI.get(`/api/v1/livechat/room?uuid=${roomUuid}`);
-    return response.data;
-}
-
-/** USER 채팅방 수정 */
-export const updateChatRoom = async (chatRoomId, data) => {
-    const response = await LiveChatAPI.put(`/api/v1/livechat/room/${chatRoomId}`, data);
-    return response.data;
-}
-
-/** USER 채팅방 삭제 */
-export const deleteChatRoom = async (chatRoomId) => {
-    await LiveChatAPI.delete(`/api/v1/livechat/room/${chatRoomId}`);
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////
-/** ADMIN 채팅방 목록 */
+/** ADMIN 채팅방 목록 - List */
 export const fetchAdminChatRooms = async () => {
     const response = await LiveChatAPI.get(`/api/v1/admin/livechat/list`);
     return response.data;
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/** USER 채팅방 조회 - id */
+export const fetchChatRoomById = async (id) => {
+    const response = await LiveChatAPI.get(`/api/v1/livechat/room/${id}`);
+    return response.data;
+}
+
+/** USER 채팅방 조회 - uuid */
+export const fetchChatRoomByUuid = async (roomUuid) => {
+    const params = { uuid: roomUuid };
+    const response = await LiveChatAPI.get(`/api/v1/livechat/room`, { params });
+    return response.data;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/** USER 채팅방 생성 */
+export const createChatRoom = async (data) => {
+    const response = await LiveChatAPI.post(`/api/v1/livechat/room`, data);
+    return response.data;
+}
+
+/** USER 채팅방 수정 */
+export const updateChatRoom = async (id, data) => {
+    const response = await LiveChatAPI.put(`/api/v1/livechat/room/${id}`, data);
+    return response.data;
+}
+
+/** USER 채팅방 삭제 */
+export const deleteChatRoom = async (id) => {
+    await LiveChatAPI.delete(`/api/v1/livechat/room/${id}`);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////
+/** 채팅방 메세지 목록 */
+export const fetchChatMessages = async (roomId = null, uuid = null) => {
+    const params = { chatRoom: roomId, uuid: uuid };
+    const response = await LiveChatAPI.get(`/api/v1/livechat/message`, { params });
+    return response.data;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
+const ApiObject = {
+    LiveChatAPI,
+    fetchChatMessages,
+    fetchChatRooms, fetchAdminChatRooms,
+    fetchChatRoomById, fetchChatRoomByUuid,
+    createChatRoom, updateChatRoom, deleteChatRoom,
+};
+export default ApiObject;
