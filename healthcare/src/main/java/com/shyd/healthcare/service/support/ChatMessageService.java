3e005c5c25ed803919991f2455d7e57965745e78
JoSihun <kepler452k@naver.com>
2023-05-22 19:29:36 +0900

    feat(service): Update Services as entities and repositories have changed
    - ChatRoomService.java:
      - Refactor combined findAllAsc() and findAllDesc() into findAll() using Sort Object
      - Add function findAll() using Pageable Object
      - Refactor combined findAllByUserIdAsc() and findAllByUserIdDesc() into findAllByUserId() using Sort Object
      - Add function findAllByUserId() using Pageable Object
      - Refactor function findByUuid()
      - Refactor function findById()
      - Fix merge create() and save() functions into create() with modified internal implementation
      - Refactor function update()
      - Refactor function delete()
    - ChatMessageService.java:
      - Refactor combined findAllByChatRoomIdAsc() and findAllByChatRoomIdDesc() into findAllByChatRoomId() using Sort Object
      - Refactor combined findAllByChatRoomUuidAsc() and findAllByChatRoomUuidDesc() into findAllByChatRoomUuid() using Sort Object
      - Refactor function findById()
      - Add function create()
      - Refactor function update()
      - Refactor function delete()

--- healthcare/src/main/java/com/shyd/healthcare/service/support/ChatMessageService.java
+++ healthcare/src/main/java/com/shyd/healthcare/service/support/ChatMessageService.java
@@ -1,90 +1,89 @@
 package com.shyd.healthcare.service.support;
 
+import com.shyd.healthcare.config.JwtTokenProvider;
 import com.shyd.healthcare.domain.support.livechat.ChatMessage;
 import com.shyd.healthcare.domain.support.livechat.ChatRoom;
-import com.shyd.healthcare.dto.support.livechat.ChatMessageRequestDto;
-import com.shyd.healthcare.dto.support.livechat.ChatMessageResponseDto;
+import com.shyd.healthcare.domain.user.User;
+import com.shyd.healthcare.dto.support.livechat.ChatMessageRequestDTO;
+import com.shyd.healthcare.dto.support.livechat.ChatMessageResponseDTO;
 import com.shyd.healthcare.repository.support.ChatMessageRepository;
 import com.shyd.healthcare.repository.support.ChatRoomRepository;
+import com.shyd.healthcare.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
-@RequiredArgsConstructor
 @Service
+@RequiredArgsConstructor
 public class ChatMessageService {
+    private final UserRepository userRepository;
+    private final JwtTokenProvider jwtTokenProvider;
     private final ChatRoomRepository chatRoomRepository;
     private final ChatMessageRepository chatMessageRepository;
 
-    /** 특정 채팅방 LiveChat Message 목록조회 - 작성순, Id 검색, List */
+    /** ChatMessage 목록조회 - 특정 ChatRoom id 검색, List */
     @Transactional
-    public List<ChatMessageResponseDto> findAllByChatRoomIdAsc(final Long chatRoomId) {
-        ChatRoom chatRoomEntity = this.chatRoomRepository.findById(chatRoomId).orElseThrow(
-                () -> new IllegalArgumentException("해당 고객지원 LiveChat Room이 존재하지 않습니다. id = " + chatRoomId));
-        // 테스트 필요
-        // Sort sort = Sort.by(Sort.Direction.ASC, "id");
-        // List<ChatMessage> chatMessageList = this.chatMessageRepository.findAllByChatRoom(chatRoomEntity, sort);
-        List<ChatMessage> chatMessageList = chatRoomEntity.getChatMessageList();
-        return chatMessageList.stream().map(ChatMessageResponseDto::new).collect(Collectors.toList());
+    public List<ChatMessageResponseDTO> findAllByChatRoomId(Long chatRoomId, String sortOption) {
+        ChatRoom chatRoom = this.chatRoomRepository.findById(chatRoomId).orElseThrow(
+                () -> new IllegalArgumentException("해당 채팅방을 찾을 수 없습니다. chatRoom_id = " + chatRoomId));
+        List<ChatMessage> chatMessages = new ArrayList<>(chatRoom.getChatMessages());
+        chatMessages.sort(Comparator.comparingLong(ChatMessage::getId));
+        if (sortOption.equalsIgnoreCase("desc")) {
+            chatMessages.sort(Comparator.comparingLong(ChatMessage::getId).reversed());
+        }
+        return chatMessages.stream().map(ChatMessageResponseDTO::new).collect(Collectors.toList());
     }
 
-    /** 특정 채팅방 LiveChat Message 목록조회 - 최신순, Id 검색, List */
+    /** ChatMessage 목록조회 - 특정 ChatRoom uuid 검색, List */
     @Transactional
-    public List<ChatMessageResponseDto> findAllByChatRoomIdDesc(final Long chatRoomId) {
-        ChatRoom chatRoomEntity = this.chatRoomRepository.findById(chatRoomId).orElseThrow(
-                () -> new IllegalArgumentException("해당 고객지원 LiveChat Room이 존재하지 않습니다. id = " + chatRoomId));
-        // 테스트 필요
-        // Sort sort = Sort.by(Sort.Direction.DESC, "id");
-        // List<ChatMessage> chatMessageList = this.chatMessageRepository.findAllByChatRoom(chatRoomEntity, sort);
-        List<ChatMessage> chatMessageList = chatRoomEntity.getChatMessageList();
-        Collections.sort(chatMessageList, Collections.reverseOrder());
-        return chatMessageList.stream().map(ChatMessageResponseDto::new).collect(Collectors.toList());
-    }
-
-    /** 특정 채팅방 LiveChat Message 목록조회 - 작성순, Uuid 검색, List */
-    @Transactional
-    public List<ChatMessageResponseDto> findAllByChatRoomUuidAsc(final String chatRoomUuid) {
-        ChatRoom chatRoomEntity = this.chatRoomRepository.findByUuid(chatRoomUuid);
-        List<ChatMessage> chatMessageList = chatRoomEntity.getChatMessageList();
-        return chatMessageList.stream().map(ChatMessageResponseDto::new).collect(Collectors.toList());
-    }
-
-    /** 특정 채팅방 LiveChat Message 목록조회 - 최신순, Uuid 검색, List */
-    @Transactional
-    public List<ChatMessageResponseDto> findAllByChatRoomUuidDesc(final String chatRoomUuid) {
-        ChatRoom chatRoomEntity = this.chatRoomRepository.findByUuid(chatRoomUuid);
-        List<ChatMessage> chatMessageList = chatRoomEntity.getChatMessageList();
-        Collections.sort(chatMessageList, Collections.reverseOrder());
-        return chatMessageList.stream().map(ChatMessageResponseDto::new).collect(Collectors.toList());
+    public List<ChatMessageResponseDTO> findAllByChatRoomUuid(String chatRoomUuid, String sortOption) {
+        ChatRoom chatRoom = this.chatRoomRepository.findByUuid(chatRoomUuid);
+        List<ChatMessage> chatMessages = new ArrayList<>(chatRoom.getChatMessages());
+        chatMessages.sort(Comparator.comparingLong(ChatMessage::getId));
+        if (sortOption.equalsIgnoreCase("desc")) {
+            chatMessages.sort(Comparator.comparingLong(ChatMessage::getId).reversed());
+        }
+        return chatMessages.stream().map(ChatMessageResponseDTO::new).collect(Collectors.toList());
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    /** LiveChat Message 조회 */
+    /** ChatMessage 조회 */
     @Transactional
-    public ChatMessageResponseDto findById(final Long id) {
-        ChatMessage entity = this.chatMessageRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("해당 고객지원 LiveChat Message가 존재하지 않습니다. id = " + id));
-        return new ChatMessageResponseDto(entity);
+    public ChatMessageResponseDTO findById(Long id) {
+        ChatMessage chatMessage = this.chatMessageRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 채팅 메세지를 찾을 수 없습니다. chatMessage_id = " + id));
+        return new ChatMessageResponseDTO(chatMessage);
     }
 
-    /** LiveChat Message 생성 */
+    /** ChatMessage 생성 */
     @Transactional
-    public Long save(final Long chatRoomId, final ChatMessageRequestDto requestDto) {
-        ChatRoom chatRoomEntity = this.chatRoomRepository.findById(chatRoomId).orElseThrow(
-                () -> new IllegalArgumentException("해당 고객지원 LiveChat Room이 존재하지 않습니다. id = " + chatRoomId));
-        requestDto.setChatRoom(chatRoomEntity);
-        return this.chatMessageRepository.save(requestDto.toEntity()).getId();
+    public Long create(String accessToken, Long chatRoomId, ChatMessageRequestDTO requestDTO) {
+        Long userId = this.jwtTokenProvider.getUserIdFromToken(accessToken.substring(7));
+        User user = this.userRepository.findById(userId).orElseThrow(
+                () -> new IllegalArgumentException("해당 유저를 찾을 수 없습니다. user_id = " + userId));
+        ChatRoom chatRoom = this.chatRoomRepository.findById(chatRoomId).orElseThrow(
+                () -> new IllegalArgumentException("해당 채팅방을 찾을 수 없습니다. chatRoom_id = " + chatRoomId));
+        requestDTO.setSender(user);
+        requestDTO.setChatRoom(chatRoom);
+        return this.chatMessageRepository.save(requestDTO.toEntity()).getId();
     }
 
-    /** LiveChat Message 삭제 */
+    /** ChatMessage 수정 */
     @Transactional
-    public void delete(final Long id) {
-        ChatMessage entity = this.chatMessageRepository.findById(id).orElseThrow(
-                () -> new IllegalArgumentException("해당 고객지원 LiveChat Message가 존재하지 않습니다. id = " + id));
-        this.chatMessageRepository.delete(entity);
+    public Long update(Long id, ChatMessageRequestDTO requestDTO) {
+        ChatMessage chatMessage = this.chatMessageRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 채팅 메세지를 찾을 수 없습니다. chatMessage_id = " + id));
+        return chatMessage.update(requestDTO);
+    }
+
+    /** ChatMessage 삭제 */
+    @Transactional
+    public void delete(Long id) {
+        ChatMessage chatMessage = this.chatMessageRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 채팅 메세지를 찾을 수 없습니다. chatMessage_id = " + id));
+        this.chatMessageRepository.delete(chatMessage);
     }
 }
