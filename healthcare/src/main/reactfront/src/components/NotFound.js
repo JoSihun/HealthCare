d50ea7fd3239cbd638dce4f385d440171bb9a5a6
JoSihun <kepler452k@naver.com>
2023-05-23 15:28:11 +0900

    refactor(NotFound.js): Update design for 404 Not Found page

--- healthcare/src/main/reactfront/src/components/NotFound.js
+++ healthcare/src/main/reactfront/src/components/NotFound.js
@@ -5,20 +5,20 @@ export default function NotFound() {
     return (
         <Container fluid>
             <Row className="my-3">
-                <Col className="col-12 col-lg-6">
+                <Col className="col-12 col-lg-6 text-center">
                     <img className="img-fluid"
-                    alt="404 Not Found"
                     // src="https://blog.thomasnet.com/hubfs/shutterstock_774749455.jpg"
-                    src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7888.jpg?w=2000"
+                    src="https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7888.jpg?w=700"
+                    alt="404 Not Found"
                     />
                 </Col>
                 <Col className="col-12 col-lg-6 d-flex justify-content-center align-items-center">
-                    <div className="border-top border-bottom border-danger border-2 rounded p-4">
+                    <div className="border-top border-bottom border-primary border-2 rounded p-4">
                         <div className="d-flex justify-content-center mb-1">
                             <h1 className="display-4 fw-bold text-secondary">Oops! Page Not Found</h1>
                         </div>
                         <div className="d-flex justify-content-center mb-2">
-                            <h4 className="fw-bold text-primary">The page you are looking for does not exist.</h4>
+                            <h4 className="fw-bold text-danger">The page you are looking for does not exist.</h4>
                         </div>
                         <div className="d-flex justify-content-center">
                             <Button className="fw-bold" variant="dark" href="/">Go Back to Homepage</Button>
