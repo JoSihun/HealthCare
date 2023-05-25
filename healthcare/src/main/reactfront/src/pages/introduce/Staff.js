cb6d23cb1d11789b88d9968b505aea153271e600
jyd12190@gmail.com <jyd12190@gmail.com>
2023-05-25 21:26:22 +0900

    refactor(Staff.js): Refactor component Staff.js
    - Refactored the Staff component to improve code readability and maintainability.
    - Updated the AddForm component to display user information and allow staff role input.
    - Updated the UserItem component to display user information in a table row.
    - Updated the UserList component to use the UserItem component and display a list of users in a table.
    - Updated the SearchForm component to improve the layout and handle user search.
    - Updated the StaffItem component to improve the layout and handle staff deletion.

--- healthcare/src/main/reactfront/src/pages/introduce/Staff.js
+++ healthcare/src/main/reactfront/src/pages/introduce/Staff.js
@@ -1,13 +1,15 @@
 import user_pic from '../../assets/images/user_pic.jpg'
 import '../../styles/Staff.css'
 import React, { useEffect, useState } from "react";
-import { Button, Card, Row, Col, Container } from "react-bootstrap";
+import { Button, Card, Row, Col, Container, Table } from "react-bootstrap";
 import SideBar from "../../components/introduce/SideBar";
 import { fetchStaffV1, deleteStaffV1, createStaffV1 } from '../../api/Introduce/StaffAPI';
 import { searchUsers } from '../../api/UserAPI';
+import { Link } from 'react-router-dom';
 
 const AddForm = (props) => {
-    const { userId, setShowAddForm } = props;
+    const { setUsers, handleShow } = props;
+    const { selectedUser, setSelectedUser } = props;
     const [value, setValue] = useState({
         staffRole: "",
     });
@@ -19,11 +21,13 @@ const AddForm = (props) => {
     }
     
     const handleCancel = async (e) => {
-        setShowAddForm(false);
+        setSelectedUser({});
+        handleShow();
+        setUsers([]);
     }
 
     const handleSubmit = async (e) => {
-        createStaffV1(userId,value)
+        createStaffV1(selectedUser.id, value)
         .then((response) => {
             console.log(response);
         }).catch((error) => {
@@ -32,64 +36,83 @@ const AddForm = (props) => {
     }
 
     return(
-        <form onSubmit={handleSubmit}>
-            <div className='form-group input-group'>
-                <label className='input-group-text'>직책</label>
-                <input className='form-control' id="staffRole" type='text' onChange={handleChange}></input>
-                <Button type='submit' variant='primary'>추가</Button>
-                <Button onClick={handleCancel} variant='danger'>취소</Button>
-            </div>
-        </form>
+        <div className='d-flex justify-content-center'>
+            <form className='w-75' onSubmit={handleSubmit}>
+                <div className='form-group input-group mb-3'>
+                    <label className='input-group-text'>이름</label>
+                    <input className='form-control' id="username" type='text' value={selectedUser.username || ""} readOnly />
+                    <label className='input-group-text'>이메일</label>
+                    <input className='form-control' id="email" type='text' value={selectedUser.email || ""} readOnly />
+                    <label className='input-group-text'>연락처</label>
+                    <input className='form-control' id="contact" type='text' value={selectedUser.contact || ""} readOnly />
+                </div>
+                <div className='form-group input-group mb-3'>
+                    <label className='input-group-text'>직책</label>
+                    <input className='form-control' id="staffRole" type='text' onChange={handleChange} value={value.staffRole} 
+                        placeholder='직책을 입력해주세요.' />
+                </div>
+                <div className='form-group d-flex justify-content-center'>
+                    <Button className='me-1' type='submit' variant='primary'style={{ width: "100px" }}>추가</Button>
+                    <Button className='ms-1' onClick={handleCancel} variant='danger'style={{ width: "100px" }}>취소</Button>
+                </div>
+            </form>
+        </div>
     );
 }
 
 const UserItem = (props) => {
-    const { user } = props;
-    const [showAddForm, setShowAddForm] = useState(false);
+    const { user, setSelectedUser } = props;
+
+    const handleClick = async (e) => {
+        setSelectedUser(user);
+    }
 
     return (
-        <div className='d-flex justify-content-between mb-2'>
-            <div>{user.username}</div>
-            <div>{user.email}</div>
-            <div>{user.contact}</div>
-            {showAddForm
-            ?
-                <AddForm userId={user.id} setShowAddForm={setShowAddForm} />
-            :
-                <Button onClick={() => { setShowAddForm(true) }} variant='warning'>+</Button>
-            }
-        </div>
+        <tr>
+            <td className='text-start'>{user.username}</td>
+            <td className='text-start'>{user.email}</td>
+            <td className='text-start'>{user.contact}</td>
+            <td className='text-center'>
+                <Button onClick={handleClick} variant="dark" style={{ width: "100px" }}>
+                    선택
+                </Button>
+            </td>
+        </tr>
     );
 }
 
 const UserList = (props) => {
-    const { users } = props;
+    const { users, setSelectedUser } = props;
 
     return (
-        <div>
-            {users.map((user, index) => (
-                <UserItem user={user} key={index} />
-            ))}
-        </div>
+        <Table responsive hover>
+            <thead>
+                <tr>
+                    <th className='col-3 text-start'>이름</th>
+                    <th className='col-3 text-start'>이메일</th>
+                    <th className='col-3 text-start'>연락처</th>
+                    <th className='col-3 text-center'>추가</th>
+                </tr> 
+            </thead>
+            <tbody>
+                {users.map((user, index) => (
+                    <UserItem key={index} user={user} setSelectedUser={setSelectedUser} />
+                ))}
+            </tbody>
+        </Table>
     );
 }
 
 const SearchForm = (props) => {
-    const [users, setUsers] = useState([]);
+    const { setUsers, handleShow } = props;
     const [value, setValue] = useState({
-        username: "",
+        username: ""
     });
-    
-    const handleChange = async (e) => {
-        e.preventDefault();
-        setValue({...value,
-            [e.target.id]: e.target.value,
-        });
-    }
 
-    const handleCancel = async (e) => {
-        e.preventDefault();
-        props.setShowAddForm(false);
+    const handleChange = async (e) => {
+        setValue({...value,
+            [e.target.id]: e.target.value
+        });
     }
 
     const handleSubmit = async (e) => {
@@ -99,38 +122,59 @@ const SearchForm = (props) => {
             setUsers(response);
         }).catch((error) => {
             console.log(error);
-        });       
+        });
     }
-    
+
     return (
-        <Card border="dark">
-            <Card.Body>
-                {users
-                ?
-                    <UserList users={users} />
-                :
-                    <h5>해당정보를 찾을수 없습니다.</h5>
-                }
-                <form onSubmit={handleSubmit}>
-                    <div className="form-group mb-3">
-                        <label htmlFor="username"><h5><strong>USERNAME</strong></h5></label>
-                        <input type="text" className="form-control" id="username" onChange={handleChange} value={ value.username } />
-                    </div>
-                    <div className="form-group d-flex justify-content-end">
-                        <Button className="me-1" variant="primary" style={{ width: "100px" }}
-                        type="submit" >검색</Button>
-                        <Button className="ms-1" variant="danger" style={{ width: "100px" }}
-                        onClick={handleCancel} >취소</Button>
-                    </div>
-                </form>
+        <form onSubmit={handleSubmit}>
+            <div className="form-group">
+                <label htmlFor="username"><h5><strong>USERNAME 검색</strong></h5></label>
+                <div className='d-flex justyfy-content-between'>
+                    <input type="text" className="form-control" id="username" onChange={handleChange} value={ value.username } />
+                    <Button className="ms-1" variant="primary" style={{ width: "100px" }} type="submit" >검색</Button>
+                </div>
+            </div>
+        </form>
+    );
+}
+
+const UserSearch = (props) => {
+    const [show, setShow] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [selectedUser, setSelectedUser] = useState({});
+
+    const handleShow = async (e) => {
+        setShow(!show);
+    }
+
+    return (show ? (
+        <Card>
+            <Card.Body className='pb-0'>
+                <Card.Title>
+                    <h4><strong>Staff 추가</strong></h4>
+                </Card.Title>
+                <hr/>
             </Card.Body>
-        </Card>        
-    );    
-    
+            <Card.Body className='pt-0 pb-4'>
+                <SearchForm setUsers={setUsers} handleShow={handleShow} />
+            </Card.Body>
+            <Card.Body className='border-top border-bottom border-2 pt-0'>
+                <UserList users={users} setSelectedUser={setSelectedUser} />
+            </Card.Body>
+            <Card.Body>
+                <AddForm selectedUser={selectedUser} setSelectedUser={setSelectedUser}
+                    handleShow={handleShow} setUsers={setUsers} />
+            </Card.Body>
+        </Card>
+    ) : (
+        <div className='d-flex justify-content-end mb-3'>
+            <Button onClick={handleShow} variant='dark' style={{ width: "100px" }}>추가</Button>
+        </div>
+    ));
 }
 
 const StaffItem = (props) => {
-    const { staffs } = props;
+    const { staff } = props;
 
     const handleDelete = async (params, e) => {
         e.preventDefault();
@@ -141,40 +185,43 @@ const StaffItem = (props) => {
             console.log(error);
         })
     }
-
-    return(
-        <Row className="my-md-3">
-            {staffs.map((staff, index) => (         
-                <Col className="col-md-6" key={index} >
-                    <Card className='w-100'>
-                        <Card.Body>
-                            <Col className="col-md-2">
-                                <img
-                                    className="rounded"
-                                    src={user_pic}
-                                    width="100"
-                                    height="100"
-                                    alt="profile"
-                                />
-                            </Col>
-                            <Col className="col-md-4">
-                                <div>역할: {staff.staffRole}</div>
-                                <div>이름: {staff.user.username}</div>
-                                <div>연락처: {staff.user.contact}</div>
-                                <div>이메일: {staff.user.email}</div>
-                            </Col>
-                            <Button className="me-1" variant="danger" style={{ width: "100px" }}
-                            onClick={(e) => { handleDelete(staff.id, e) }}>삭제</Button>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            ))}
-        </Row>
-    )
+    
+    return (
+        <Card>
+            <Card.Body className='pb-2'>
+                <div className='d-flex justify-content-start'>
+                    <div className='me-2'>
+                        <img
+                        className="rounded"
+                        src={user_pic}
+                        width="100"
+                        height="100"
+                        alt="profile"
+                        />
+                    </div>
+                    <div className='ms-2'>
+                        <div>이&nbsp;&nbsp;&nbsp;름: {staff.user.username}</div>
+                        <div>직&nbsp;&nbsp;&nbsp;책: {staff.staffRole}</div>
+                        <div>연락처: {staff.user.contact}</div>
+                        <div>이메일: {staff.user.email}</div>
+                    </div>
+                </div>
+                <div className='d-flex justify-content-end'>
+                    <Link className='text-secondary' style={{ textDecoration: "none" }}>
+                        <small>수정</small>
+                    </Link>
+                    &nbsp;/&nbsp;
+                    <Link className='text-secondary' style={{ textDecoration: "none" }}
+                        onClick={(e) => { handleDelete(staff.id, e) }}>
+                        <small>삭제</small>
+                    </Link>
+                </div>
+            </Card.Body>
+        </Card>
+    );
 }
 
-function Staff() {
-    const [showAddForm, setShowAddForm] = useState(false);
+const StaffList = (props) => {
     const [staffs, setStaffs] = useState([]);
 
     useEffect(() => {
@@ -187,34 +234,38 @@ function Staff() {
     }, []);
 
     return (
-        <Container fluid>
-        <Row className="justify-content-center">
-            <Col className="col-md-2 mx-2 my-4">
-                <SideBar />
-            </Col>
-            <Col className="col-md-9 mx-2 my-4">
-                <Card>
-                    <Card.Body>
-                        <Card.Title><h2><strong>Staff</strong></h2></Card.Title>
-                        <hr/>
-                        <StaffItem staffs={staffs} />                                        
-                    </Card.Body>
-                </Card>
-                {!showAddForm
-                    ?
-                        <div className="d-flex justify-content-end mt-3">
-                            <Button className="me-1" variant="dark" style={{ width: "100px" }}
-                            onClick={() => { setShowAddForm(!showAddForm) }}>추가</Button>
-                        </div>
-                    :
-                        <div className="mt-4">
-                            <SearchForm setShowAddForm={setShowAddForm} />
-                        </div>
-                }
-            </Col>
-        </Row>
-    </Container>
+        <Card className='mb-3'>
+            <Card.Body className='pb-0'>
+                <Card.Title>
+                    <h2><strong>Staff</strong></h2>
+                </Card.Title>
+                <hr/>
+            </Card.Body>
+            <Card.Body className='py-0'>
+                <Row className='justify-content-start'>
+                    {staffs.map((staff, index) => (
+                        <Col className='col-12 col-lg-6 mb-3' key={index}>
+                            <StaffItem staff={staff} />
+                        </Col>
+                    ))}
+                </Row>
+            </Card.Body>
+        </Card>
     );
 }
 
-export default Staff;
+export default function Staff() {
+    return (
+        <Container fluid>
+            <Row className='justify-content-center mt-3'>
+                <Col className='col-12 col-lg-2 mb-3'>
+                    <SideBar />
+                </Col>
+                <Col className='col-12 col-lg-9 mb-3'>
+                    <StaffList />
+                    <UserSearch />
+                </Col>
+            </Row>
+        </Container>
+    );
+}
